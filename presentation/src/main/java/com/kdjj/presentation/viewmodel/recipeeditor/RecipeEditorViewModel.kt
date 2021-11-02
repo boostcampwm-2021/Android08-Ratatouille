bc18936cb41311c2c496522d7eb5e8fc7706196f
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-03 02:13:59 +0900

    feat: title 및 title state 추가, title test code 작성 #5

--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeEditorViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeEditorViewModel.kt
@@ -1,6 +1,8 @@
 package com.kdjj.presentation.viewmodel.recipeeditor
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.kdjj.domain.usecase.FetchRecipeTypesUseCase
 import com.kdjj.domain.usecase.SaveRecipeUseCase
 import javax.inject.Inject
@@ -9,4 +11,14 @@ class RecipeEditorViewModel @Inject constructor(
     private val fetchRecipeTypes: FetchRecipeTypesUseCase,
     private val saveRecipe: SaveRecipeUseCase
 ) : ViewModel() {
+
+    val title = MutableLiveData<String>()
+    val titleState = title.switchMap {
+        MutableLiveData(validateTitle(it))
+    }
+
+    private fun validateTitle(title: String): Boolean {
+        // to-do
+        return true
+    }
 }
