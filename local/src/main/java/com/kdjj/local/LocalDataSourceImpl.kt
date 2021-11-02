5b5be3029ef2ba46808bc6fcde26c64dd85e33c2
dongkey <utdhkim94@gmail.com>
2021-11-02 17:06:56 +0900

    feat: domain 모듈 추가, saveRecipe 인터페이스 및 확장클래스 추가

---
+++ local/src/main/java/com/kdjj/local/LocalDataSourceImpl.kt
@@ -0,0 +1,10 @@
+package com.kdjj.local
+
+import com.kdjj.domain.model.Recipe
+
+class LocalDataSourceImpl: LocalDataSource {
+
+    override suspend fun saveRecipe(recipe: Recipe) {
+        TODO("Not yet implemented")
+    }
+}
