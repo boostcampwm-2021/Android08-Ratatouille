2b63e4eeb2a837c0cd414d8c8f0cb4e1c5bbf4de
K.H.Park <svclaw2000@gmail.com>
2021-11-02 16:58:07 +0900

    feat: initialize recipe editor activity #20

---
+++ presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
@@ -0,0 +1,16 @@
+package com.kdjj.presentation.view.recipeeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kdjj.presentation.R
+import com.kdjj.presentation.databinding.ActivityRecipeEditorBinding
+
+class RecipeEditorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecipeEditorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_editor)
+    }
+}
