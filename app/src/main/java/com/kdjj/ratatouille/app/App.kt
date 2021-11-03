6b235845edbbeeaf82761025107c8a49b4728b34
K.H.Park <svclaw2000@gmail.com>
2021-11-03 20:15:38 +0900

    feat: migrate to hilt #8

--- app/src/main/java/com/kdjj/ratatouille/app/App.kt
+++ app/src/main/java/com/kdjj/ratatouille/app/App.kt
@@ -1,22 +1,7 @@
 package com.kdjj.ratatouille.app
 
 import android.app.Application
-import com.kdjj.presentation.di.PresentationComponentProvider
-import com.kdjj.presentation.di.PresentationSubComponent
-import com.kdjj.ratatouille.di.AppComponent
-import com.kdjj.ratatouille.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
-class App : Application(), PresentationComponentProvider {
-
-    val appComponent: AppComponent by lazy {
-        initializeComponent()
-    }
-
-    private fun initializeComponent(): AppComponent {
-        return DaggerAppComponent.factory().create(applicationContext)
-    }
-
-    override fun providePresentationComponent(): PresentationSubComponent {
-        return appComponent.presentationComponent().create()
-    }
-}
+@HiltAndroidApp
+class App : Application()
