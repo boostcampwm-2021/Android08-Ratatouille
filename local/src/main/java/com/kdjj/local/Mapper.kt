abc68b4a276222777b5e11d1a2227c944daf9e88
dongkey <utdhkim94@gmail.com>
2021-11-03 19:51:02 +0900

    feat: Domain Model에서 Local Model로 변환하는 Mapper 추가

--- local/src/main/java/com/kdjj/local/Mapper.kt
+++ local/src/main/java/com/kdjj/local/Mapper.kt
@@ -1,4 +1,28 @@
 package com.kdjj.local
 
-class Mapper {
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.RecipeStep
+import com.kdjj.local.model.RecipeMetaEntity
+import com.kdjj.local.model.RecipeStepEntity
+
+fun domainToEntity(recipe: Recipe): RecipeMetaEntity {
+    return RecipeMetaEntity(
+        null,
+        recipe.title,
+        recipe.stuff,
+        recipe.imgPath,
+        recipe.type.id.toLong()
+    )
+}
+
+fun domainToEntity(step: RecipeStep, recipeMetaID: Long): RecipeStepEntity {
+     return RecipeStepEntity(
+        null,
+         step.name,
+         step.type,
+         step.description,
+         step.imgPath,
+         step.seconds,
+         recipeMetaID
+    )
 }
