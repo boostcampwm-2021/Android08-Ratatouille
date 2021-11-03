c7db0da1fd1ad8960bb369ce4af4f30b5f7e1fe4
K.H.Park <svclaw2000@gmail.com>
2021-11-03 14:29:35 +0900

    feat: change interface to data class #6

--- domain/src/main/java/com/kdjj/domain/model/Recipe.kt
+++ domain/src/main/java/com/kdjj/domain/model/Recipe.kt
@@ -1,27 +1,9 @@
 package com.kdjj.domain.model
 
-interface Recipe {
-    val title: String
-    val type: Type
-    val stuff: String
-    val imgPath: String
-    val stepList: List<Step>
-
-    interface Step {
-        val name: String
-        val type: Type
-        val description: String
-        val imgPath: String
-        val seconds: Int
-
-        enum class Type {
-            FRY,
-        }
-    }
-
-    interface Type {
-        val id: Int
-        val title: String
-    }
-}
-
+data class Recipe(
+    val title: String,
+    val type: RecipeType,
+    val stuff: String,
+    val imgPath: String,
+    val stepList: List<RecipeStep>
+)
--- domain/src/main/java/com/kdjj/domain/repository/RecipeRepository.kt
+++ domain/src/main/java/com/kdjj/domain/repository/RecipeRepository.kt
@@ -1,10 +1,11 @@
 package com.kdjj.domain.repository
 
 import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.RecipeType
 
 interface RecipeRepository {
 
     suspend fun saveRecipe(recipe: Recipe): Result<Boolean>
 
-    suspend fun fetchRecipeTypes(): Result<List<Recipe.Type>>
+    suspend fun fetchRecipeTypes(): Result<List<RecipeType>>
 }
