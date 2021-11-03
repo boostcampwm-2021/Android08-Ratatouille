ad880bb062f4404c696cc08239ef10dad060893c
dongkey <utdhkim94@gmail.com>
2021-11-03 15:32:41 +0900

    feat: RecipeMetaEntity, RecipeStepEntity, RecipeTypeEntity, RecipeEntity 추가

---
+++ local/src/main/java/com/kdjj/local/RecipeEntity.kt
@@ -0,0 +1,20 @@
+package com.kdjj.local
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class RecipeEntity(
+    @Embedded val recipeMeta: RecipeMetaEntity,
+
+    @Relation(
+        parentColumn = "recipeMetaId",
+        entityColumn = "parentRecipeId"
+    )
+    val recipeType: RecipeTypeEntity,
+
+    @Relation(
+        parentColumn = "recipeMetaId",
+        entityColumn = "parentRecipeId"
+    )
+    val steps: List<RecipeStepEntity>
+)
