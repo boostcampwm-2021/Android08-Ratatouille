0adac684f62cd814b4582fdefda2ec1fb3e99b6f
K.H.Park <svclaw2000@gmail.com>
2021-11-03 15:00:40 +0900

    feat: apply UseCase and Request interface. close #6

---
+++ domain/src/main/java/com/kdjj/domain/request/RecipeRequest.kt
@@ -0,0 +1,5 @@
+package com.kdjj.domain.request
+
+import com.kdjj.domain.model.Recipe
+
+class RecipeRequest(val recipe: Recipe) : Request
--- domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypesUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypesUseCase.kt
@@ -1,11 +1,14 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.model.RecipeType
 import com.kdjj.domain.repository.RecipeRepository
+import com.kdjj.domain.request.EmptyRequest
 import javax.inject.Inject
 
 class FetchRecipeTypesUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
-) {
+) : UseCase<EmptyRequest, List<RecipeType>> {
 
-    suspend operator fun invoke() = recipeRepository.fetchRecipeTypes()
+    override suspend fun invoke(request: EmptyRequest): Result<List<RecipeType>> =
+        recipeRepository.fetchRecipeTypes()
 }
