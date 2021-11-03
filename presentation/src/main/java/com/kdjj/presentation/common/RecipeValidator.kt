8035a1bf2fd6d57579ee776249d50993efd0ef13
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-03 19:09:15 +0900

    feat: model Validator 클래스 추가 #5

---
+++ presentation/src/main/java/com/kdjj/presentation/common/RecipeValidator.kt
@@ -0,0 +1,16 @@
+package com.kdjj.presentation.common
+
+import javax.inject.Inject
+
+class RecipeValidator @Inject constructor() {
+
+    fun validateTitle(title: String): Boolean {
+        // to-do
+        return true
+    }
+
+    fun validateStuff(stuff: String): Boolean {
+        // to-do
+        return true
+    }
+}
