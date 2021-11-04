83be8758773df433f13edfe018479b86354ce7f2
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "feat: add recipe type to recipe editor #20"
    This reverts commit b3d4fe2d5d89174eb6491cafcd89ed07387cbad2.

--- presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
@@ -2,7 +2,6 @@ package com.kdjj.presentation.view.recipeeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
 import com.kdjj.presentation.databinding.ActivityRecipeEditorBinding
@@ -16,12 +15,5 @@ class RecipeEditorActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbarEditor)
         setTitle(R.string.addRecipe)
-
-        val adapter = ArrayAdapter(
-            this,
-            R.layout.item_editor_recipe_type,
-            arrayOf("한식", "양식", "중식", "일식")
-        )
-        binding.spinnerEditorRecipeType.adapter = adapter
     }
 }
