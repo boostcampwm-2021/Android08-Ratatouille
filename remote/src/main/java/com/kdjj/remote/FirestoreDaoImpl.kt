d2dffb91f3b389f862ef335e1d63ebf20d2e07df
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-04 18:24:34 +0900

    feat: firebaseDao 선언 및 구현 #11

---
+++ remote/src/main/java/com/kdjj/remote/FirestoreDaoImpl.kt
@@ -0,0 +1,29 @@
+package com.kdjj.remote
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import com.kdjj.domain.model.RecipeType
+import com.kdjj.remote.entity.RecipeTypeEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class FirestoreDaoImpl(
+	private val firestore: FirebaseFirestore
+) : FirestoreDao {
+	
+	override suspend fun fetchRecipeTypes(): List<RecipeType> =
+		withContext(Dispatchers.IO) {
+			firestore.collection(RECIPE_TYPE_COLLECTION_ID)
+				.get()
+				.await()
+				.map { queryDocumentSnapshot ->
+					entityToDomain(queryDocumentSnapshot.toObject<RecipeTypeEntity>())
+				}
+		}
+	
+	companion object {
+		
+		const val RECIPE_TYPE_COLLECTION_ID = "recipeType"
+	}
+}
