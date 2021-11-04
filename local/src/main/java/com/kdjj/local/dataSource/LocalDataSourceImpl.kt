43eda516ef9e1f7553d342bcd84fba65dcf315a3
dongkey <utdhkim94@gmail.com>
2021-11-04 16:46:15 +0900

    feat: Gallery uri ByteArray로 변환 함수 구현

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
@@ -1,12 +1,14 @@
 package com.kdjj.local.dataSource
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.util.Log
 import com.kdjj.domain.model.Recipe
 import com.kdjj.local.DAO.RecipeDAO
+import com.kdjj.local.FileSaveHelper
 import com.kdjj.local.domainToEntity
 import com.kdjj.local.model.RecipeTypeEntity
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.lang.Exception
@@ -22,7 +24,10 @@ import java.lang.Exception
  * InputStream으로 이미지 불러와서 -> ByteStream으로 변환해서 -> Internal Storage 저장
  *
  * **/
-class LocalDataSourceImpl(private val recipeDatabase: RecipeDAO, private val filesDir: File) :
+class LocalDataSourceImpl(
+    private val recipeDatabase: RecipeDAO,
+    private val fileSaveHelper: FileSaveHelper
+) :
     LocalDataSource {
 
     //Recipe 저장
@@ -61,4 +66,8 @@ class LocalDataSourceImpl(private val recipeDatabase: RecipeDAO, private val fil
             }
         }
     }
+
+    //Gallery Image Uri를 BtyeArray로 변환
+    override suspend fun localUriToByteArray(uri: String): Result<ByteArray> =
+        fileSaveHelper.convertToByteArray(uri)
 }
