87e7311ad922afd1a8a2181506a4639e660fd74e
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-04 21:16:54 +0900

    feat: RecipeItem.kt 추가, RecipeMapper.kt 추가, 레시피 저장 로직 #5

---
+++ presentation/src/main/java/com/kdjj/presentation/common/RecipeMapper.kt
@@ -0,0 +1,65 @@
+package com.kdjj.presentation.common
+
+import android.content.Context
+import android.provider.Settings
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.RecipeStep
+import com.kdjj.domain.model.RecipeStepType
+import com.kdjj.presentation.model.RecipeItem
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import java.lang.Exception
+import javax.inject.Inject
+
+class RecipeMapper @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+
+    suspend fun recipeItemListToRecipe(recipeItemList: List<RecipeItem>): Recipe {
+        val recipeMetaModel = recipeItemList[0] as RecipeItem.RecipeMetaModel
+        val recipeStepList = recipeItemList.subList(1, recipeItemList.lastIndex)
+
+        recipeStepList.asFlow()
+            .map { recipeStepModelToRecipeStep(it as RecipeItem.RecipeStepModel) }
+            .flowOn(Dispatchers.IO)
+            .toList().also {
+                return Recipe(
+                    recipeId = generateId(),
+                    title = recipeMetaModel.liveTitle.value ?: "",
+                    type= recipeMetaModel.liveRecipeType.value ?: throw Exception(""),
+                    stuff = recipeMetaModel.liveStuff.value ?: "",
+                    imgPath = recipeMetaModel.liveRecipeImgPath.value ?: "",
+                    stepList = it,
+                    uploaderId = recipeMetaModel.uploadId,
+                    viewCount = recipeMetaModel.viewCount,
+                    isFavorite = recipeMetaModel.isFavorite,
+                    createTime = System.currentTimeMillis()
+                )
+            }
+    }
+
+    fun recipeStepModelToRecipeStep(recipeStepModel: RecipeItem.RecipeStepModel): RecipeStep {
+        return RecipeStep(
+            generateId(),
+            recipeStepModel.liveName.value ?: "",
+            recipeStepModel.liveType.value ?: RecipeStepType.FRY,
+            recipeStepModel.liveDescription.value ?: "",
+            recipeStepModel.liveImgPath.value ?: "",
+            calculateSeconds(
+                recipeStepModel.liveTimerMin.value ?: 0,
+                recipeStepModel.liveTimerSec.value ?: 0
+            )
+        )
+    }
+
+    private fun calculateSeconds(min: Int, sec: Int): Int{
+        return min*60 + sec
+    }
+
+    private fun generateId() = getDeviceId() + System.currentTimeMillis()
+
+    private fun getDeviceId(): String {
+        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+    }
+}
--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeEditorViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeEditorViewModel.kt
@@ -1,59 +1,94 @@
 package com.kdjj.presentation.viewmodel.recipeeditor
 
 import android.net.Uri
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
+import androidx.lifecycle.*
+import com.kdjj.domain.model.RecipeStepType
+import com.kdjj.domain.model.RecipeType
+import com.kdjj.domain.request.EmptyRequest
+import com.kdjj.domain.request.RecipeRequest
+import com.kdjj.domain.usecase.UseCase
+import com.kdjj.presentation.common.RecipeMapper
 import com.kdjj.presentation.common.RecipeStepValidator
 import com.kdjj.presentation.common.RecipeValidator
-import com.kdjj.presentation.model.RecipeStepModel
+import com.kdjj.presentation.model.RecipeItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
 
+@HiltViewModel
 class RecipeEditorViewModel @Inject constructor(
     private val recipeValidator: RecipeValidator,
     private val recipeStepValidator: RecipeStepValidator,
+    private val recipeSaveUseCase: UseCase<RecipeRequest, Boolean>,
+    private val recipeTypesUseCase: UseCase<EmptyRequest, List<RecipeType>>,
+    private val recipeMapper: RecipeMapper
 ) : ViewModel() {
 
-    //livedata naming convention
-    val liveTitle = MutableLiveData<String>()
-    val liveTitleState = liveTitle.switchMap {
-        MutableLiveData(recipeValidator.validateTitle(it))
+    private var _liveRecipeItemList = MutableLiveData<List<RecipeItem>>()
+    val liveRecipeItemList = _liveRecipeItemList
+
+    val stepTypes = RecipeStepType.values()
+    lateinit var recipeTypes : List<RecipeType>
+
+    init {
+        _liveRecipeItemList.value = listOf(createEmptyRecipeMetaModel(), RecipeItem.PlusButton)
     }
 
-    val liveCategoryPosition = MutableLiveData<Int>(0) // spinner position
-
-    val liveStuff = MutableLiveData<String>()
-    val liveStuffState = liveStuff.switchMap {
-        MutableLiveData(recipeValidator.validateStuff(it))
-    }
-
-    val liveRecipeImgPath = MutableLiveData<String>()
-
-    fun setRecipeImg(uri: Uri) {
-        liveRecipeImgPath.value = uri.path
-    }
-
-    private val _liveStepList = MutableLiveData<List<RecipeStepModel>>(listOf())
-    val liveStepList: LiveData<List<RecipeStepModel>> = _liveStepList
-
-    fun addRecipeStep() {
-        _liveStepList.value?.let {
-            _liveStepList.value = it.plus(createEmptyRecipeStepModel())
+    fun fetchRecipeTypes() {
+        viewModelScope.launch {
+            val res = recipeTypesUseCase.invoke(EmptyRequest())
+            res.onSuccess {
+                recipeTypes = it
+            }
+            res.onFailure {
+                // todo
+            }
         }
     }
 
-    private fun createEmptyRecipeStepModel(): RecipeStepModel {
+    fun setRecipeImg(uri: Uri) {
+        _liveRecipeItemList.value?.let {
+            (it[0] as RecipeItem.RecipeMetaModel).liveRecipeImgPath.value = uri.path
+        }
+    }
+
+    fun addRecipeStep() {
+        _liveRecipeItemList.value?.let {
+            _liveRecipeItemList.value = it.subList(0, it.lastIndex) + createEmptyRecipeStepModel() + it.last()
+        }
+    }
+
+    private fun createEmptyRecipeMetaModel(): RecipeItem.RecipeMetaModel {
+        val liveTitle = MutableLiveData<String>("")
+        val liveStuff = MutableLiveData<String>("")
+        val liveRecipeImgPath = MutableLiveData<String>()
+        val liveCategoryPosition = MutableLiveData(0)
+        return RecipeItem.RecipeMetaModel(
+            liveTitle = liveTitle,
+            liveStuff = liveStuff,
+            liveRecipeImgPath = liveRecipeImgPath,
+            liveRecipeTypeInt = liveCategoryPosition,
+            liveRecipeType = liveCategoryPosition.switchMap { MutableLiveData(recipeTypes[it]) },
+
+            liveStuffState = liveStuff.switchMap { MutableLiveData(recipeValidator.validateStuff(it)) },
+            liveTitleState = liveTitle.switchMap { MutableLiveData(recipeValidator.validateTitle(it)) }
+        )
+    }
+
+    private fun createEmptyRecipeStepModel(): RecipeItem.RecipeStepModel {
         val liveName =  MutableLiveData("")
         val liveDescription = MutableLiveData("")
         val liveTimerMin = MutableLiveData(0)
         val liveTimerSec = MutableLiveData(0)
-        return RecipeStepModel(
+        val liveTypeInt = MutableLiveData(0)
+        return RecipeItem.RecipeStepModel(
             liveName = liveName,
             liveDescription = liveDescription,
             liveTimerMin = liveTimerMin,
             liveTimerSec = liveTimerSec,
+            liveTypeInt = liveTypeInt,
+            liveType = liveTypeInt.switchMap { MutableLiveData(stepTypes[it]) },
 
             liveNameState = liveName.switchMap { MutableLiveData(recipeStepValidator.validateName(it)) },
             liveDescriptionState = liveDescription.switchMap { MutableLiveData(recipeStepValidator.validateDescription(it)) },
@@ -63,36 +98,48 @@ class RecipeEditorViewModel @Inject constructor(
     }
 
     fun removeRecipeStep(position: Int) {
-        _liveStepList.value?.let {
-            if (it.size <= 1) _liveStepList.value = emptyList()
-            else {
-                if (position == 0) _liveStepList.value = it.subList(1, it.size)
-                else if (position == it.lastIndex) _liveStepList.value = it.subList(0, it.lastIndex)
-                else _liveStepList.value = it.subList(0, position) + it.subList(position + 1, it.size)
+        if (position > 0) {
+            _liveRecipeItemList.value?.let {
+                _liveRecipeItemList.value = it.subList(0, position) + it.subList(position + 1, it.size)
             }
         }
     }
 
     fun changeRecipeStepPosition(from: Int, to: Int) {
-        _liveStepList.value?.let {
+        _liveRecipeItemList.value?.let {
             if (it.isNotEmpty()) {
                 Collections.swap(it, from, to)
-                _liveStepList.value = it
+                _liveRecipeItemList.value = it
+            }
+        }
+    }
+
+    fun saveRecipe() {
+        viewModelScope.launch {
+            liveRecipeItemList.value?.let {
+                val res = recipeSaveUseCase.invoke( RecipeRequest(recipeMapper.recipeItemListToRecipe(it)))
+                res.onSuccess { /*화면 이동*/ }
+                res.onFailure {  }
             }
         }
     }
 
     private fun checkAllValidate(): Boolean {
         // check recipe meta
-        if (liveTitleState.value != true || liveStuffState.value != true) {
-            return false
-        }
-
-        // check step list
-        liveStepList.value?.forEach {
-            if (it.liveNameState.value != true || it.liveDescriptionState.value != true ||
-                it.liveTimerMinState.value != true || it.liveTimerSecState.value != true) {
-                return false
+        _liveRecipeItemList.value?.forEach {
+            when (it) {
+                is RecipeItem.RecipeMetaModel -> {
+                    if (it.liveTitleState.value != true || it.liveStuffState.value != true) {
+                        return false
+                    }
+                }
+                is RecipeItem.RecipeStepModel -> {
+                    if (it.liveNameState.value != true || it.liveDescriptionState.value != true ||
+                        it.liveTimerMinState.value != true || it.liveTimerSecState.value != true) {
+                        return false
+                    }
+                }
+                else -> {}
             }
         } ?: return false
 
