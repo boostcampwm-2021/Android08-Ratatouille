29e452fbfc76c6134a684c85d244373f8a5c7095
dongkey <utdhkim94@gmail.com>
2021-11-04 16:46:15 +0900

    feat: Gallery uri ByteArray로 변환 함수 구현

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
@@ -10,4 +10,6 @@ interface LocalDataSource {
     suspend fun saveRecipeTypes()
 
     suspend fun getRecipeTypes(): Result<List<RecipeTypeEntity>>
+
+    suspend fun localUriToByteArray(uri: String): Result<ByteArray>
 }
