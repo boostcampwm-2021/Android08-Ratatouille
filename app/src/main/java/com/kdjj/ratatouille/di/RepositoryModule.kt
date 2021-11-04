1936a445816583c0bc17d50401b6275c9aacbeb7
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "fix: provide FetchRecipeTypesUseCase #5"
    This reverts commit 5fc1f9a953b67925f451cbc5d69a805f8269048d.

--- app/src/main/java/com/kdjj/ratatouille/di/RepositoryModule.kt
+++ app/src/main/java/com/kdjj/ratatouille/di/RepositoryModule.kt
@@ -1,9 +1,6 @@
 package com.kdjj.ratatouille.di
 
-import com.kdjj.domain.model.RecipeType
-import com.kdjj.domain.request.EmptyRequest
 import com.kdjj.domain.request.RecipeRequest
-import com.kdjj.domain.usecase.FetchRecipeTypesUseCase
 import com.kdjj.domain.usecase.SaveRecipeUseCase
 import com.kdjj.domain.usecase.UseCase
 import dagger.Binds
@@ -19,7 +16,4 @@ abstract class RepositoryModule {
 //    abstract fun provideRecipeRepository(recipeRepositoryImpl: RecipeRepositoryImpl): RecipeRepository
     @Binds
     abstract fun provideSaveRecipeUseCase(saveRecipeUseCase: SaveRecipeUseCase): UseCase<RecipeRequest, Boolean>
-//
-//    @Binds
-//    abstract fun provideFetchRecipeTypesUseCase(fetchRecipeTypesUseCase: FetchRecipeTypesUseCase): UseCase<EmptyRequest, List<RecipeType>>
 }
