36a15fabd6ddccf305a14eac0316b63008be3bf8
dongkey <utdhkim94@gmail.com>
2021-11-04 15:26:32 +0900

    refactor: Local 모듈 Mapper 수정

--- local/src/main/java/com/kdjj/local/Mapper.kt
+++ local/src/main/java/com/kdjj/local/Mapper.kt
@@ -7,22 +7,25 @@ import com.kdjj.local.model.RecipeStepEntity
 
 fun domainToEntity(recipe: Recipe): RecipeMetaEntity {
     return RecipeMetaEntity(
-        null,
+        recipe.recipeId,
         recipe.title,
         recipe.stuff,
         recipe.imgPath,
+        recipe.uploaderId,
+        recipe.isFavorite,
+        recipe.createTime,
         recipe.type.id.toLong()
     )
 }
 
-fun domainToEntity(step: RecipeStep, recipeMetaID: Long): RecipeStepEntity {
-     return RecipeStepEntity(
-        null,
-         step.name,
-         step.type,
-         step.description,
-         step.imgPath,
-         step.seconds,
-         recipeMetaID
+fun domainToEntity(step: RecipeStep, recipeMetaID: String): RecipeStepEntity {
+    return RecipeStepEntity(
+        step.stepId,
+        step.name,
+        step.type,
+        step.description,
+        step.imgPath,
+        step.seconds,
+        recipeMetaID
     )
 }
