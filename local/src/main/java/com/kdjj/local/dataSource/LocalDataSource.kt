b41700d4f1867bafb0e3f297f0fe1be758eb5a76
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "feat: LocalDataSoure에 Recipe(RecipeMeta, RecipeStep)저장 기능 구현, Local Database에 저장된 모든 RecipeType 읽어오기 기능 구현"
    This reverts commit 2f9ca8358957985c9d4c8c6a8cd3106e778ee2e1.

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
@@ -1,13 +1,8 @@
 package com.kdjj.local.dataSource
 
 import com.kdjj.domain.model.Recipe
-import com.kdjj.local.model.RecipeTypeEntity
 
 interface LocalDataSource {
 
-    suspend fun saveRecipe(recipe: Recipe)
-
-    suspend fun saveRecipeTypes()
-
-    suspend fun getRecipeTypes(): List<RecipeTypeEntity>
+    suspend fun saveRecipe(recipe: Recipe): Result<Boolean>
 }
--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
@@ -1,31 +1,11 @@
 package com.kdjj.local.dataSource
 
-import android.util.Log
 import com.kdjj.domain.model.Recipe
-import com.kdjj.local.DAO.RecipeDAO
-import com.kdjj.local.domainToEntity
-import com.kdjj.local.model.RecipeEntity
-import com.kdjj.local.model.RecipeTypeEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.kdjj.local.dataSource.LocalDataSource
 
-class LocalDataSourceImpl(private val recipeDatabase: RecipeDAO) : LocalDataSource {
+class LocalDataSourceImpl: LocalDataSource {
 
-    override suspend fun saveRecipe(recipe: Recipe) {
-        withContext(Dispatchers.IO) {
-            val recipeMetaID = recipeDatabase.insertRecipeMeta(domainToEntity(recipe))
-            recipe.stepList.map { recipeStep ->
-                recipeDatabase.insertRecipeStep(domainToEntity(recipeStep, recipeMetaID))
-            }
-        }
-    }
-
-    override suspend fun saveRecipeTypes() {
-    }
-
-    override suspend fun getRecipeTypes(): List<RecipeTypeEntity> {
-        return withContext(Dispatchers.IO) {
-            return@withContext recipeDatabase.getAllRecipeTypes()
-        }
+    override suspend fun saveRecipe(recipe: Recipe): Result<Boolean> {
+        TODO("Not yet implemented")
     }
 }
