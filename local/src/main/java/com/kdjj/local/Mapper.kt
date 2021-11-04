dd3798f7396149631bebf7b8c5aee8b18a44c3f6
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "feat: Domain Model에서 Local Model로 변환하는 Mapper 추가"
    This reverts commit a3a7089f820d2ff6d120afc28f396488211d4ab4.

--- local/src/main/java/com/kdjj/local/Mapper.kt
+++ local/src/main/java/com/kdjj/local/Mapper.kt
@@ -1,28 +1,4 @@
 package com.kdjj.local
 
-import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.RecipeStep
-import com.kdjj.local.model.RecipeMetaEntity
-import com.kdjj.local.model.RecipeStepEntity
-
-fun domainToEntity(recipe: Recipe): RecipeMetaEntity {
-    return RecipeMetaEntity(
-        null,
-        recipe.title,
-        recipe.stuff,
-        recipe.imgPath,
-        recipe.type.id.toLong()
-    )
-}
-
-fun domainToEntity(step: RecipeStep, recipeMetaID: Long): RecipeStepEntity {
-     return RecipeStepEntity(
-        null,
-         step.name,
-         step.type,
-         step.description,
-         step.imgPath,
-         step.seconds,
-         recipeMetaID
-    )
+class Mapper {
 }
