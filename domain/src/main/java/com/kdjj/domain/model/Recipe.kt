c1deeb4fa5e3ea278249f43797bab67f876ecfef
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-04 12:38:19 +0900

    feat: Recipe 필요 프로퍼티 추가

--- domain/src/main/java/com/kdjj/domain/model/Recipe.kt
+++ domain/src/main/java/com/kdjj/domain/model/Recipe.kt
@@ -1,9 +1,14 @@
 package com.kdjj.domain.model
 
 data class Recipe(
-    val title: String,
-    val type: RecipeType,
-    val stuff: String,
-    val imgPath: String,
-    val stepList: List<RecipeStep>
+	val recipeId: String,
+	val title: String,
+	val type: RecipeType,
+	val stuff: String,
+	val imgPath: String,
+	val stepList: List<RecipeStep>,
+	val uploaderId: String?,
+	val viewCount: Int,
+	val isFavorite: Boolean,
+	val createTime: Long
 )
