5e9bf5f6257a2afc42f176caea8cd8c761d28788
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "feat: initialize recipe editor activity #20"
    This reverts commit 81fbfd8c00b01868724f5e118978c33a6e0b21eb.

--- presentation/src/main/java/com/kdjj/presentation/view/splash/LaunchActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/splash/LaunchActivity.kt
@@ -1,16 +1,13 @@
 package com.kdjj.presentation.view.splash
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kdjj.presentation.R
-import com.kdjj.presentation.view.recipeeditor.RecipeEditorActivity
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this, RecipeEditorActivity::class.java))
     }
 }
