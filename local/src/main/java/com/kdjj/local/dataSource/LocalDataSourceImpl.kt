a0dbaf72841c78306e39a481dfe29cfe1f18cf61
dongkey <utdhkim94@gmail.com>
2021-11-04 15:25:37 +0900

    refactor: Local 모듈 DataModel 수정

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
@@ -1,10 +1,12 @@
 package com.kdjj.local.dataSource
 
+import android.util.Log
 import com.kdjj.domain.model.Recipe
 import com.kdjj.local.DAO.RecipeDAO
 import com.kdjj.local.domainToEntity
 import com.kdjj.local.model.RecipeTypeEntity
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.lang.Exception
@@ -26,9 +28,9 @@ class LocalDataSourceImpl(private val recipeDatabase: RecipeDAO, private val fil
     override suspend fun saveRecipe(recipe: Recipe): Result<Boolean> {
         return withContext(Dispatchers.IO) {
             return@withContext try {
-                val recipeMetaID = recipeDatabase.insertRecipeMeta(domainToEntity(recipe))
+                recipeDatabase.insertRecipeMeta(domainToEntity(recipe))
                 recipe.stepList.forEach { recipeStep ->
-                    recipeDatabase.insertRecipeStep(domainToEntity(recipeStep, recipeMetaID))
+                    recipeDatabase.insertRecipeStep(domainToEntity(recipeStep, recipe.recipeId))
                 }
                 Result.success(true)
             } catch (e: Exception) {
--- local/src/main/java/com/kdjj/local/model/RecipeMetaEntity.kt
+++ local/src/main/java/com/kdjj/local/model/RecipeMetaEntity.kt
@@ -2,7 +2,6 @@ package com.kdjj.local.model
 
 import androidx.room.*
 
-// https://developer.android.com/training/data-storage/room/relationships
 @Entity(
     tableName = "RecipeMeta",
     foreignKeys = [
@@ -15,11 +14,14 @@ import androidx.room.*
     ]
 )
 data class RecipeMetaEntity(
-    @PrimaryKey(autoGenerate = true)
-    val recipeMetaId: Long? = null,
+    @PrimaryKey
+    val recipeMetaId: String,
     val title: String,
     val stuff: String,
     var imgPath: String,
+    val uploaderId:String?,
+    var isFavorite:  Boolean,
+    val createTime: Long,
     val recipeTypeId: Long
 )
 
