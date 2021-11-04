0195f404acc260899f75dd00490d49f647958864
Keich.Park <svclaw2000@gmail.com>
2021-11-05 00:43:43 +0900

    Revert "feat: add toolbar for RecipeEditorActivity #20"
    This reverts commit 7252adac1b058d2404bd70d63d722ad3388d44ff.

--- presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
@@ -12,8 +12,5 @@ class RecipeEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_editor)
-
-        setSupportActionBar(binding.toolbarEditor)
-        setTitle(R.string.addRecipe)
     }
 }
