bd940316429a15037aa27763e3e5b8f0af81a3cf
dongkey <utdhkim94@gmail.com>
2021-11-04 17:48:23 +0900

    feat: Image ByteArray Internal 저장소에 저장한후 경로 반환 기능 구현

--- local/src/main/java/com/kdjj/local/FileSaveHelper.kt
+++ local/src/main/java/com/kdjj/local/FileSaveHelper.kt
@@ -2,15 +2,20 @@ package com.kdjj.local
 
 import android.content.ContentResolver
 import android.net.Uri
+import android.os.FileUtils
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 
 class FileSaveHelper(private val fileDir: File, private val contentResolver: ContentResolver) {
 
     suspend fun convertToByteArray(uri: String): Result<ByteArray> {
-        return withContext(Dispatchers.IO){
+        return withContext(Dispatchers.IO) {
             return@withContext try {
                 val inputStream = contentResolver.openInputStream(Uri.parse(uri))
                 val byteArray = inputStream?.readBytes()
@@ -22,5 +27,20 @@ class FileSaveHelper(private val fileDir: File, private val contentResolver: Con
         }
     }
 
-
+    suspend fun convertToInternalStorageUri(
+        byteArray: ByteArray,
+        fileName: String
+    ): Result<String> {
+        return withContext(Dispatchers.IO) {
+            val filePath = "$fileDir/$fileName"
+            var fos: FileOutputStream? = null
+            return@withContext try {
+                fos = FileOutputStream(filePath)
+                fos.write(byteArray)
+                Result.success(filePath)
+            } catch (e: Exception) {
+                Result.failure(Exception(e))
+            }
+        }
+    }
 }
