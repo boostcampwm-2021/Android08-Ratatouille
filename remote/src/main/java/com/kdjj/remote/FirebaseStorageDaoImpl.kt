da62def5b5a8144d76a5e80b81dd8a96b837b381
dongkey <utdhkim94@gmail.com>
2021-11-08 22:56:41 +0900

    feat: remote 레이어에 FirebaseStorageDaoImpl 추가 #43

--- remote/src/main/java/com/kdjj/remote/FirebaseStorageDaoImpl.kt
+++ remote/src/main/java/com/kdjj/remote/FirebaseStorageDaoImpl.kt
@@ -1,4 +1,26 @@
 package com.kdjj.remote
 
-class FirebaseStorageDaoImpl {
+import com.google.firebase.storage.FirebaseStorage
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import javax.inject.Inject
+
+class FirebaseStorageDaoImpl @Inject constructor(
+    private val storage: FirebaseStorage
+) : FirebaseStorageDao {
+
+    override suspend fun fetchRecipeImage(uri: String): Result<ByteArray> {
+        val maxSize: Long = 3238 * 3238
+        return withContext(Dispatchers.IO) {
+            try {
+                val byteArray = storage.getReferenceFromUrl(uri).getBytes(maxSize).await()
+                Result.success(byteArray)
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
 }
