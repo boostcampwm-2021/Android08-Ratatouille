55da2d870472d11f7f9877b2eb9947d862ebebe6
dongkey <utdhkim94@gmail.com>
2021-11-09 01:32:57 +0900

    feat: FirebaseStorageDao에 uploadRecipeImage() 추가 #43

--- remote/src/main/java/com/kdjj/remote/FirebaseStorageDao.kt
+++ remote/src/main/java/com/kdjj/remote/FirebaseStorageDao.kt
@@ -3,4 +3,6 @@ package com.kdjj.remote
 interface FirebaseStorageDao {
 
     suspend fun fetchRecipeImage(uri: String): Result<ByteArray>
+
+    suspend fun uploadRecipeImage(uri: String): Result<String>
 }
--- remote/src/main/java/com/kdjj/remote/FirebaseStorageDaoImpl.kt
+++ remote/src/main/java/com/kdjj/remote/FirebaseStorageDaoImpl.kt
@@ -1,24 +1,40 @@
 package com.kdjj.remote
 
-import com.google.firebase.storage.FirebaseStorage
+import android.net.Uri
+import com.google.firebase.storage.StorageReference
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.lang.Exception
 import javax.inject.Inject
 
 class FirebaseStorageDaoImpl @Inject constructor(
-    private val storage: FirebaseStorage
+    private val storageRef: StorageReference
 ) : FirebaseStorageDao {
 
     override suspend fun fetchRecipeImage(uri: String): Result<ByteArray> {
         val tenMegaByte: Long = 10485760L
         return withContext(Dispatchers.IO) {
             try {
-                val byteArray = storage.getReferenceFromUrl(uri).getBytes(tenMegaByte).await()
+                val byteArray = storageRef.storage.getReferenceFromUrl(uri).getBytes(tenMegaByte).await()
                 Result.success(byteArray)
             } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
+
+    override suspend fun uploadRecipeImage(uri: String): Result<String>{
+        return withContext(Dispatchers.IO){
+            try {
+                val file = Uri.fromFile(File(uri))
+                val refer = storageRef.child("images/${file.lastPathSegment}")
+                refer.putFile(file).await()
+                val newUri = refer.downloadUrl.await()
+                Result.success(newUri.toString())
+            } catch (e: Exception){
                 Result.failure(e)
             }
         }
