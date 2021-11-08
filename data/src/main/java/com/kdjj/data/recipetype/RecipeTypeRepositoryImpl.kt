0f665653ead651ea41f3955bc3754703c48a7297
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-08 16:27:40 +0900

    feat: RecipeTypeRepository 구현

--- data/src/main/java/com/kdjj/data/recipetype/RecipeTypeRepositoryImpl.kt
+++ data/src/main/java/com/kdjj/data/recipetype/RecipeTypeRepositoryImpl.kt
@@ -1,9 +1,22 @@
 package com.kdjj.data.recipetype
 
+import com.kdjj.domain.model.RecipeType
 import com.kdjj.domain.repository.RecipeTypeRepository
 
 class RecipeTypeRepositoryImpl(
 	private val localDataSource: RecipeTypeLocalDataSource,
 	private val remoteDataSource: RecipeTypeRemoteDataSource
 ) : RecipeTypeRepository {
+	
+	override suspend fun fetchRecipeTypes(): Result<List<RecipeType>> {
+		return remoteDataSource.fetchRecipeTypes()
+	}
+	
+	override suspend fun saveRecipeType(): Result<Boolean> {
+		return localDataSource.saveRecipeType()
+	}
+	
+	override suspend fun getRecipeTypes(): Result<List<RecipeType>> {
+		return localDataSource.getRecipeTypes()
+	}
 }
--- domain/src/main/java/com/kdjj/domain/repository/RecipeTypeRepository.kt
+++ domain/src/main/java/com/kdjj/domain/repository/RecipeTypeRepository.kt
@@ -1,4 +1,12 @@
 package com.kdjj.domain.repository
 
+import com.kdjj.domain.model.RecipeType
+
 interface RecipeTypeRepository {
+	
+	suspend fun fetchRecipeTypes(): Result<List<RecipeType>>
+	
+	suspend fun saveRecipeType(): Result<Boolean>
+	
+	suspend fun getRecipeTypes(): Result<List<RecipeType>>
 }
