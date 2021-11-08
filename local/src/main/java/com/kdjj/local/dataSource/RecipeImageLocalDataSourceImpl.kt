eb71e3e18762523ba5e47f290a05cbb4f5c5da45
dongkey <utdhkim94@gmail.com>
2021-11-08 20:58:21 +0900

    feat: local layer에 RecipeImageLocalDataSourceImpl 구현 #43

--- local/src/main/java/com/kdjj/local/dataSource/RecipeImageLocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeImageLocalDataSourceImpl.kt
@@ -1,4 +1,20 @@
 package com.kdjj.local.dataSource
 
-class RecipeImageLocalDataSourceImpl {
+import com.kdjj.data.recipetype.RecipeImageLocalDataSource
+import com.kdjj.local.FileSaveHelper
+import javax.inject.Inject
+
+class RecipeImageLocalDataSourceImpl @Inject constructor(private val fileSaveHelper: FileSaveHelper) :
+    RecipeImageLocalDataSource {
+
+    override suspend fun convertToByteArray(uri: String): Result<ByteArray> {
+        return fileSaveHelper.convertToByteArray(uri)
+    }
+
+    override suspend fun convertToInternalStorageUri(
+        byteArray: ByteArray,
+        fileName: String
+    ): Result<String> {
+        return fileSaveHelper.convertToInternalStorageUri(byteArray, fileName)
+    }
 }
