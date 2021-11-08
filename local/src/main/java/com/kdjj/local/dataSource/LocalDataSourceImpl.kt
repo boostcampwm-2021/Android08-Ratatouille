7ad1c79726aedfdfc77550dba3fb802a357ced34
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-08 16:12:24 +0900

    refactor(LocalDataSource): 필요없는 함수 제거

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSourceImpl.kt
@@ -1,11 +1,9 @@
 package com.kdjj.local.dataSource
 
 import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.RecipeType
 import com.kdjj.local.DAO.RecipeDAO
 import com.kdjj.local.FileSaveHelper
 import com.kdjj.local.domainToEntity
-import com.kdjj.local.entityToDomain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -29,23 +27,6 @@ class LocalDataSourceImpl(
 					)
 				}
 				Result.success(true)
-			} catch (e: Exception) {
-				Result.failure(Exception(e.message))
-			}
-		}
-	
-	//Recipe Type 저장하기
-	override suspend fun saveRecipeTypes() {
-		//TODO
-	}
-	
-	//Recipe Type 읽어오기
-	override suspend fun getRecipeTypes(): Result<List<RecipeType>> =
-		withContext(Dispatchers.IO) {
-			return@withContext try {
-				val recipeTypeList = recipeDatabase.getAllRecipeTypes()
-					.map { entityToDomain(it) }
-				Result.success(recipeTypeList)
 			} catch (e: Exception) {
 				Result.failure(Exception(e.message))
 			}
