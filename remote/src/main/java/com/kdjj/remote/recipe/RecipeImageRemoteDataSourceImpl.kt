4c4a57c0ba0ab4b624a1014b796fada79e47b4f5
dongkey <utdhkim94@gmail.com>
2021-11-08 23:26:07 +0900

    feat: remote 레이어에 RecipeImageRemoteDataSourceImpl 구현 #43

--- remote/src/main/java/com/kdjj/remote/recipe/RecipeImageRemoteDataSourceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/recipe/RecipeImageRemoteDataSourceImpl.kt
@@ -1,4 +1,14 @@
 package com.kdjj.remote.recipe
 
-class RecipeImageRemoteDataSourceImpl {
+import com.kdjj.data.recipeimage.RecipeImageRemoteDataSource
+import com.kdjj.remote.FirebaseStorageDao
+import javax.inject.Inject
+
+class RecipeImageRemoteDataSourceImpl @Inject constructor(
+    private val firebaseStorageDao: FirebaseStorageDao
+): RecipeImageRemoteDataSource {
+
+    override suspend fun fetchRecipeImage(uri: String): Result<ByteArray> {
+        return firebaseStorageDao.fetchRecipeImage(uri)
+    }
 }
