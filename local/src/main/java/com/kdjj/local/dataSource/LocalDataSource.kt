bff9feccc4f0a564d888eb60700dd5754163e58a
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-08 16:12:24 +0900

    refactor(LocalDataSource): 필요없는 함수 제거

--- local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
+++ local/src/main/java/com/kdjj/local/dataSource/LocalDataSource.kt
@@ -1,18 +1,12 @@
 package com.kdjj.local.dataSource
 
 import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.RecipeType
-import com.kdjj.local.model.RecipeTypeEntity
 
 interface LocalDataSource {
-
-    suspend fun saveRecipe(recipe: Recipe): Result<Boolean>
-
-    suspend fun saveRecipeTypes()
-
-    suspend fun getRecipeTypes(): Result<List<RecipeType>>
-
-    suspend fun localUriToByteArray(uri: String): Result<ByteArray>
-
-    suspend fun byteArrayToLocalUri(byteArray: ByteArray, fileName: String): Result<String>
+	
+	suspend fun saveRecipe(recipe: Recipe): Result<Boolean>
+	
+	suspend fun localUriToByteArray(uri: String): Result<ByteArray>
+	
+	suspend fun byteArrayToLocalUri(byteArray: ByteArray, fileName: String): Result<String>
 }
