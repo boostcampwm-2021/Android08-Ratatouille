e866d68a4be678d371db37e9179a45c4e9e9a4ab
dongkey <utdhkim94@gmail.com>
2021-11-08 23:27:26 +0900

    feat: RecipeImageRepository에 convertToByteArrayRemote 함수 추가 #43

--- domain/src/main/java/com/kdjj/domain/repository/RecipeImageRepository.kt
+++ domain/src/main/java/com/kdjj/domain/repository/RecipeImageRepository.kt
@@ -2,7 +2,9 @@ package com.kdjj.domain.repository
 
 interface RecipeImageRepository {
 
-    suspend fun convertToByteArray(uri: String): Result<ByteArray>
+    suspend fun convertToByteArrayRemote(uri: String): Result<ByteArray>
+
+    suspend fun convertToByteArrayLocal(uri: String): Result<ByteArray>
 
     suspend fun convertToInternalStorageUri(
         byteArray: ByteArray,
