78988bc82e9fb0d86e614914d372a8f026b2e4cc
K.H.Park <svclaw2000@gmail.com>
2021-11-09 00:34:39 +0900

    feat: add glide to image view

--- presentation/src/main/java/com/kdjj/presentation/view/adapter/BindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/adapter/BindingAdapter.kt
@@ -1,10 +1,12 @@
 package com.kdjj.presentation.view.adapter
 
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.kdjj.presentation.R
 
 @Suppress("UNCHECKED_CAST")
@@ -23,4 +25,10 @@ fun Spinner.typeSpinnerItems(items: List<String>?) {
         items
     )
     adapter = spinnerAdapter
+}
+
+@BindingAdapter("app:glideSrc")
+fun ImageView.glideSrc(src: String?) {
+    src ?: return
+    Glide.with(context).load(src).into(this)
 }
