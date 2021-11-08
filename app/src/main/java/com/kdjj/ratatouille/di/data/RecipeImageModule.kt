6bced72699f8ee7bdfe0854ba373874a83dd99f5
dongkey <utdhkim94@gmail.com>
2021-11-08 23:52:12 +0900

    feat: RecipeImageModule에 RecipeRemoteDataSource 의존성 주입 추가 #43

--- app/src/main/java/com/kdjj/ratatouille/di/data/RecipeImageModule.kt
+++ app/src/main/java/com/kdjj/ratatouille/di/data/RecipeImageModule.kt
@@ -2,10 +2,18 @@ package com.kdjj.ratatouille.di.data
 
 import android.content.ContentResolver
 import android.content.Context
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.kdjj.data.recipeimage.RecipeImageLocalDataSource
+import com.kdjj.data.recipeimage.RecipeImageRemoteDataSource
 import com.kdjj.data.recipeimage.RecipeImageRepositoryImpl
 import com.kdjj.domain.repository.RecipeImageRepository
 import com.kdjj.local.dataSource.RecipeImageLocalDataSourceImpl
+import com.kdjj.remote.FirebaseStorageDao
+import com.kdjj.remote.FirebaseStorageDaoImpl
+import com.kdjj.remote.recipe.RecipeImageRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -25,9 +33,22 @@ abstract class RecipeImageModule {
 
     @Binds
     @Singleton
+    abstract fun bindRecipeImageRemoteDataSource(recipeImageRemoteDataSourceImpl: RecipeImageRemoteDataSourceImpl): RecipeImageRemoteDataSource
+
+    @Binds
+    @Singleton
     abstract fun bindRecipeImageLocalRepository(recipeImageRepositoryImpl: RecipeImageRepositoryImpl): RecipeImageRepository
 
+    @Binds
+    @Singleton
+    abstract fun bindFireStorageDao(firebaseStorageDaoImpl: FirebaseStorageDaoImpl): FirebaseStorageDao
+
+
     companion object {
+
+        @Provides
+        @Singleton
+        fun provideFirebaseStorage(): FirebaseStorage = Firebase.storage
 
         @Provides
         @Singleton
