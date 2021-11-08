23beef102ce28a1f3d680ccc3638bdbc87cc6bfe
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-08 17:37:22 +0900

    feat: RecipeEntity.kt, RecipeStepEntity.kt 추가, mapper 추가 #38

--- remote/src/main/java/com/kdjj/remote/Mapper.kt
+++ remote/src/main/java/com/kdjj/remote/Mapper.kt
@@ -1,6 +1,10 @@
 package com.kdjj.remote
 
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.RecipeStep
 import com.kdjj.domain.model.RecipeType
+import com.kdjj.remote.entity.RecipeEntity
+import com.kdjj.remote.entity.RecipeStepEntity
 import com.kdjj.remote.entity.RecipeTypeEntity
 
 fun entityToDomain(recipeTypeEntity: RecipeTypeEntity): RecipeType {
@@ -9,3 +13,31 @@ fun entityToDomain(recipeTypeEntity: RecipeTypeEntity): RecipeType {
 		recipeTypeEntity.title
 	)
 }
+
+fun entityToDomain(recipeEntity: RecipeEntity): Recipe {
+	return Recipe(
+		recipeEntity.recipeId,
+		recipeEntity.title,
+		entityToDomain(recipeEntity.type),
+		recipeEntity.stuff,
+		recipeEntity.imgPath,
+		recipeEntity.stepList.map { entityToDomain(it) },
+		recipeEntity.authorId,
+		recipeEntity.viewCount,
+		false,
+		recipeEntity.createTime,
+		recipeEntity.state
+	)
+}
+
+fun entityToDomain(recipeStepEntity: RecipeStepEntity): RecipeStep {
+	return RecipeStep(
+		recipeStepEntity.stepId,
+		recipeStepEntity.name,
+		recipeStepEntity.type,
+		recipeStepEntity.description,
+		recipeStepEntity.imgPath,
+		recipeStepEntity.seconds
+	)
+}
+
