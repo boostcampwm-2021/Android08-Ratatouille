e8faa2814755c9423aadce9fed71d9a4ff0eec15
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-09 11:52:02 +0900

    feat: 서버 검색 레시피 리스트 UseCase #40

--- domain/src/main/java/com/kdjj/domain/request/FetchRemoteSearchRecipeListRequest.kt
+++ domain/src/main/java/com/kdjj/domain/request/FetchRemoteSearchRecipeListRequest.kt
@@ -2,4 +2,5 @@ package com.kdjj.domain.request
 
 data class FetchRemoteSearchRecipeListRequest(
     val keyword: String,
+    val lastVisibleTitle: String,
 ) : Request
---
+++ domain/src/main/java/com/kdjj/domain/usecase/FetchRemoteSearchRecipeListUseCase.kt
@@ -0,0 +1,14 @@
+package com.kdjj.domain.usecase
+
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.repository.RecipeListRepository
+import com.kdjj.domain.request.FetchRemoteSearchRecipeListRequest
+import javax.inject.Inject
+
+class FetchRemoteSearchRecipeListUseCase @Inject constructor(
+    private val recipeListRepository: RecipeListRepository,
+): UseCase<FetchRemoteSearchRecipeListRequest, @JvmSuppressWildcards List<Recipe>>{
+
+    override suspend fun invoke(request: FetchRemoteSearchRecipeListRequest): Result<List<Recipe>> =
+        recipeListRepository.fetchRemoteSearchRecipeListAfter(request.keyword, request.lastVisibleTitle)
+}
