5583df0ce7b8bd4ccdc70bec61f30e9384abe4d2
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-09 16:08:43 +0900

    feat: RecipeRepository 생성 #46

---
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.kdjj.local.dataSource
+
+import com.kdjj.data.recipe.RecipeLocalDataSource
+import com.kdjj.domain.model.Recipe
+import com.kdjj.local.DAO.RecipeDAO
+import com.kdjj.local.toEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class RecipeLocalDataSourceImpl(
+	private val recipeDatabase: RecipeDAO
+) : RecipeLocalDataSource {
+	
+	override suspend fun saveRecipe(recipe: Recipe): Result<Boolean> =
+		withContext(Dispatchers.IO) {
+			try {
+				recipeDatabase.insertRecipeMeta(recipe.toEntity())
+				recipe.stepList.forEachIndexed { idx, recipeStep ->
+					recipeDatabase.insertRecipeStep(
+						recipeStep.toEntity(recipe.recipeId, idx + 1)
+					)
+				}
+				Result.success(true)
+			} catch (e: Exception) {
+				Result.failure(Exception(e.message))
+			}
+		}
+}
