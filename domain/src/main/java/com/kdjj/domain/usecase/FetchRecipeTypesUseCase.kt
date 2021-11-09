b8eb9461defe41778da79f2f819f5a26b1936708
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-09 17:04:44 +0900

    refactor(FetchRecipeTypesUseCase): 레포지토리 변경으로 인한 코드 수정 #46

--- domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypesUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypesUseCase.kt
@@ -1,14 +1,25 @@
 package com.kdjj.domain.usecase
 
 import com.kdjj.domain.model.RecipeType
-import com.kdjj.domain.repository.RecipeRepository
+import com.kdjj.domain.repository.RecipeTypeRepository
 import com.kdjj.domain.request.EmptyRequest
 import javax.inject.Inject
 
 class FetchRecipeTypesUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+	private val recipeTypeRepository: RecipeTypeRepository
 ) : UseCase<EmptyRequest, @JvmSuppressWildcards List<RecipeType>> {
-
-    override suspend fun invoke(request: EmptyRequest): Result<List<RecipeType>> =
-        recipeRepository.fetchRecipeTypes()
+	
+	override suspend fun invoke(request: EmptyRequest): Result<List<RecipeType>> {
+		val remoteResult = recipeTypeRepository.fetchRemoteRecipeTypes()
+		return when {
+			remoteResult.isSuccess -> {
+				remoteResult.also {
+					recipeTypeRepository.saveRecipeType(it.getOrThrow())
+				}
+			}
+			else -> {
+				recipeTypeRepository.fetchLocalRecipeTypes()
+			}
+		}
+	}
 }
