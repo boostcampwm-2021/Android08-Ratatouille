b61ecc8d8e326363dbbe7480e77d69d578ff7a88
dongkey <utdhkim94@gmail.com>
2021-11-09 18:56:43 +0900

    test: test RecipeImageRepositoryImpl #52

--- data/src/test/java/com/kdjj/data/recipeimage/RecipeImageRepositoryImplTest.kt
+++ data/src/test/java/com/kdjj/data/recipeimage/RecipeImageRepositoryImplTest.kt
@@ -1,24 +1,69 @@
 package com.kdjj.data.recipeimage
 
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class RecipeImageRepositoryImplTest {
 
-    @Test
-    fun convertToByteArrayRemote() {
+    lateinit var mockRecipeImageLocalDataSource: RecipeImageLocalDataSource
+    lateinit var mockRecipeImageRemoteDataSource: RecipeImageRemoteDataSource
+    lateinit var recipeImageRepositoryImpl: RecipeImageRepositoryImpl
+
+    private val testUri = "this is test uri"
+    private val testByteArray = testUri.toByteArray()
+
+    @Before
+    fun setup() {
+        mockRecipeImageLocalDataSource = mock(RecipeImageLocalDataSource::class.java)
+        mockRecipeImageRemoteDataSource = mock(RecipeImageRemoteDataSource::class.java)
+        recipeImageRepositoryImpl = RecipeImageRepositoryImpl(
+            mockRecipeImageLocalDataSource,
+            mockRecipeImageRemoteDataSource
+        )
     }
 
     @Test
-    fun convertToByteArrayLocal() {
+    fun convertToByteArrayRemote_getRemoteImageByteArray_true(): Unit = runBlocking {
+        //given
+        `when`(mockRecipeImageRemoteDataSource.fetchRecipeImage(testUri)).thenReturn(Result.success(testByteArray))
+        //when
+        val byteArrayResult = recipeImageRepositoryImpl.convertToByteArrayRemote(testUri)
+        //then
+        assertEquals(testByteArray, byteArrayResult.getOrNull())
     }
 
     @Test
-    fun convertToRemoteStorageUri() {
+    fun convertToByteArrayLocal_getLocalImageByteArray_true(): Unit = runBlocking {
+        //given
+        `when`(mockRecipeImageLocalDataSource.convertToByteArray(testUri)).thenReturn(Result.success(testByteArray))
+        //when
+        val byteArrayResult = recipeImageRepositoryImpl.convertToByteArrayLocal(testUri)
+        //then
+        assertEquals(testByteArray, byteArrayResult.getOrNull())
     }
 
     @Test
-    fun convertToLocalStorageUri() {
+    fun convertToRemoteStorageUri_getRemoteImageUri_true(): Unit = runBlocking {
+        //given
+        `when`(mockRecipeImageRemoteDataSource.uploadRecipeImage(testUri)).thenReturn(Result.success(testUri))
+        //when
+        val newImagePathResult = recipeImageRepositoryImpl.convertToRemoteStorageUri(testUri)
+        //then
+        assertEquals(testUri, newImagePathResult.getOrNull())
+    }
+
+    @Test
+    fun convertToLocalStorageUri_getLocalImageUri_true(): Unit = runBlocking {
+        //given
+        `when`(mockRecipeImageLocalDataSource.convertToInternalStorageUri(testByteArray, "fileName")).thenReturn(Result.success(testUri))
+        //when
+        val newImagePathResult = recipeImageRepositoryImpl.convertToLocalStorageUri(testByteArray, "fileName")
+        //then
+        assertEquals(testUri, newImagePathResult.getOrNull())
     }
 }
