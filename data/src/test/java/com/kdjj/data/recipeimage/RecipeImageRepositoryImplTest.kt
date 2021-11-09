4115c9f96112cb2885f0346dd64b960a732355d7
dongkey <utdhkim94@gmail.com>
2021-11-09 18:31:48 +0900

    build: mockito 의존성 추가 #52

---
+++ data/src/test/java/com/kdjj/data/recipeimage/RecipeImageRepositoryImplTest.kt
@@ -0,0 +1,24 @@
+package com.kdjj.data.recipeimage
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class RecipeImageRepositoryImplTest {
+
+    @Test
+    fun convertToByteArrayRemote() {
+    }
+
+    @Test
+    fun convertToByteArrayLocal() {
+    }
+
+    @Test
+    fun convertToRemoteStorageUri() {
+    }
+
+    @Test
+    fun convertToLocalStorageUri() {
+    }
+}
