64cd0eee2f48a83ba37997725f3d83bd173fe1e5
dongkey <utdhkim94@gmail.com>
2021-11-09 15:26:30 +0900

    refactor(FileSavaHelper): bytearray를 bitmap으로 변환후 리사이즈하고 로컬저장하도록 수정

--- local/src/main/java/com/kdjj/local/FileSaveHelper.kt
+++ local/src/main/java/com/kdjj/local/FileSaveHelper.kt
@@ -1,6 +1,8 @@
 package com.kdjj.local
 
 import android.content.ContentResolver
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.FileUtils
 import android.util.Log
@@ -32,14 +34,39 @@ class FileSaveHelper @Inject constructor(
         byteArray: ByteArray,
         fileName: String
     ): Result<String> = withContext(Dispatchers.IO) {
-        val filePath = "$fileDir/$fileName.jpeg"
         var fos: FileOutputStream? = null
         try {
+            val filePath = "$fileDir/${fileName}.png"
             fos = FileOutputStream(filePath)
-            fos.write(byteArray)
+            val bitmap = convertByteArrayToBitmap(byteArray)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 85, fos)
             Result.success(filePath)
         } catch (e: Exception) {
+            e.printStackTrace()
             Result.failure(e)
         }
+    }
+
+    private fun convertByteArrayToBitmap(byteArray: ByteArray): Bitmap{
+        val options = BitmapFactory.Options()
+        options.inSampleSize = 4
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+
+        val width = 300
+        val height = 300
+
+        var bmpWidth = bitmap.width.toFloat()
+        var bmpHeight = bitmap.height.toFloat()
+
+        if (bmpWidth > width) {
+            val scale = width / (bmpWidth / 100)
+            bmpWidth *= (scale / 100)
+            bmpHeight *= (scale / 100)
+        } else if(bmpHeight > height){
+            val scale = width / (bmpHeight / 100)
+            bmpWidth *= (scale / 100)
+            bmpHeight *= (scale / 100)
+        }
+        return Bitmap.createScaledBitmap(bitmap, bmpWidth.toInt(), bmpHeight.toInt(), true)
     }
 }
