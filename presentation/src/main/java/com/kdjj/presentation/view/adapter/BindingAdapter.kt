c29b9672e8e4ab6c46ab55ad97d5acb42188bbad
K.H.Park <svclaw2000@gmail.com>
2021-11-10 00:32:53 +0900

    refactor: remove unnecessary let #49

--- presentation/src/main/java/com/kdjj/presentation/view/adapter/BindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/adapter/BindingAdapter.kt
@@ -12,7 +12,7 @@ import com.kdjj.presentation.R
 @Suppress("UNCHECKED_CAST")
 @BindingAdapter("app:submitList")
 fun <T, VH : RecyclerView.ViewHolder> RecyclerView.submitList(list: List<T>?) {
-    (adapter as? ListAdapter<T, VH>)?.let { it.submitList(list) }
+    (adapter as? ListAdapter<T, VH>)?.submitList(list)
 }
 
 @BindingAdapter("app:typeSpinnerItems")
