481148e8b5ec6a9f2bc9d07d7db10b5cfd245aed
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-11 01:06:36 +0900

    refactor: 레시피 넣는 함수 생성으로 인한 코드 수정 #46

--- local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
@@ -3,23 +3,17 @@ package com.kdjj.local.dataSource
 import com.kdjj.data.recipe.RecipeLocalDataSource
 import com.kdjj.domain.model.Recipe
 import com.kdjj.local.dao.RecipeDao
-import com.kdjj.local.toEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class RecipeLocalDataSourceImpl(
-	private val recipeDatabase: RecipeDao
+	private val recipeDao: RecipeDao
 ) : RecipeLocalDataSource {
 	
 	override suspend fun saveRecipe(recipe: Recipe): Result<Boolean> =
 		withContext(Dispatchers.IO) {
 			try {
-				recipeDatabase.insertRecipeMeta(recipe.toEntity())
-				recipe.stepList.forEachIndexed { idx, recipeStep ->
-					recipeDatabase.insertRecipeStep(
-						recipeStep.toEntity(recipe.recipeId, idx + 1)
-					)
-				}
+				recipeDao.insertRecipe(recipe)
 				Result.success(true)
 			} catch (e: Exception) {
 				Result.failure(Exception(e.message))
