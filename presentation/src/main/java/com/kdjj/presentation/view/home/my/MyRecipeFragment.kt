1cc10a5026b6f55af1d12f5b202562c00a226c32
dongkey <utdhkim94@gmail.com>
2021-11-10 21:35:08 +0900

    feat: MyRecipeFragment Toolbar 구현 #58

--- presentation/src/main/java/com/kdjj/presentation/view/home/my/MyRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/my/MyRecipeFragment.kt
@@ -5,14 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
+import com.kdjj.presentation.databinding.FragmentMyRecipeBinding
 
 class MyRecipeFragment : Fragment() {
 
+    private var _binding: FragmentMyRecipeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_recipe, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_recipe, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolBar()
+    }
+
+    private fun initToolBar() {
+        binding.toolbarMyRecipe.apply {
+            setTitle(R.string.myRecipeTitle)
+            inflateMenu(R.menu.toolbar_menu_search_item)
+        }
     }
 }
