b0cfa3f598e51fff9428b2a83defe948e13512e9
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-10 16:28:56 +0900

    refactor(RecipeTypeEntity): 레시피 기본 타입 서버와 통일 #46

--- local/src/main/java/com/kdjj/local/model/RecipeTypeEntity.kt
+++ local/src/main/java/com/kdjj/local/model/RecipeTypeEntity.kt
@@ -1,24 +1,23 @@
 package com.kdjj.local.model
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.kdjj.domain.model.RecipeType
-import com.kdjj.local.model.RecipeMetaEntity
 
 @Entity(tableName = "RecipeType")
 data class RecipeTypeEntity(
-    @PrimaryKey
-    val recipeTypeId: Long,
-    val title: String
-){
-    companion object{
-        val defaultTypes = mutableListOf<RecipeTypeEntity>(
-            RecipeTypeEntity(1, "한식"),
-            RecipeTypeEntity(2, "중식"),
-            RecipeTypeEntity(3, "양식"),
-            RecipeTypeEntity(4, "일식"),
-            RecipeTypeEntity(5, "기타"),
-        )
-    }
+	@PrimaryKey
+	val recipeTypeId: Long,
+	val title: String
+) {
+	
+	companion object {
+		
+		val defaultTypes = mutableListOf(
+			RecipeTypeEntity(1, "기타"),
+			RecipeTypeEntity(2, "한식"),
+			RecipeTypeEntity(3, "중식"),
+			RecipeTypeEntity(4, "양식"),
+			RecipeTypeEntity(5, "일식"),
+		)
+	}
 }
