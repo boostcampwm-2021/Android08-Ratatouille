142f1c2ae5b5f6e94e3a9cf0461d19223f485697
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-10 22:41:01 +0900

    feat: RecipeDao RecipeTypeDao로 기능 분리 #46

---
+++ local/src/main/java/com/kdjj/local/dao/RecipeTypeDao.kt
@@ -0,0 +1,17 @@
+package com.kdjj.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kdjj.local.model.RecipeTypeEntity
+
+@Dao
+interface RecipeTypeDao {
+	
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertRecipeType(recipeType: RecipeTypeEntity)
+	
+	@Query("SELECT * FROM RecipeType")
+	suspend fun getAllRecipeTypeList(): List<RecipeTypeEntity>
+}
