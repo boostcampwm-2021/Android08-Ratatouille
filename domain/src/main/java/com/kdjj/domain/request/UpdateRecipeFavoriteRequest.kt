522baf49c5bac8de162c518d3c357881718039ea
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-11 03:59:17 +0900

    refactor: Request 자료형 데이터 클래스로 변경 #46

--- domain/src/main/java/com/kdjj/domain/request/UpdateRecipeFavoriteRequest.kt
+++ domain/src/main/java/com/kdjj/domain/request/UpdateRecipeFavoriteRequest.kt
@@ -2,4 +2,4 @@ package com.kdjj.domain.request
 
 import com.kdjj.domain.model.Recipe
 
-class UpdateRecipeFavoriteRequest(val recipe: Recipe) : Request
+data class UpdateRecipeFavoriteRequest(val recipe: Recipe) : Request
