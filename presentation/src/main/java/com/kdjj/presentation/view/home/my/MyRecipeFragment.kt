ad19ec2c827b49c43c9f91c5d26de17be38fca03
dongkey <utdhkim94@gmail.com>
2021-11-10 23:15:06 +0900

    feat: 레시피 정렬을 위한 Radio Button 구현 #56

--- presentation/src/main/java/com/kdjj/presentation/view/home/my/MyRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/my/MyRecipeFragment.kt
@@ -1,6 +1,8 @@
 package com.kdjj.presentation.view.home.my
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,12 +28,29 @@ class MyRecipeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolBar()
+        initRadioButtons()
     }
 
     private fun initToolBar() {
         binding.toolbarMyRecipe.apply {
-            setTitle(R.string.myRecipeTitle)
+            setTitle(R.string.myRecipe)
             inflateMenu(R.menu.toolbar_menu_search_item)
+        }
+    }
+
+    private fun initRadioButtons() {
+        binding.radioGroup.setOnCheckedChangeListener { group, _ ->
+            when (group.checkedRadioButtonId) {
+                R.id.radioButton_orderByDate -> {
+                    Log.d("aaa", "1")
+                }
+                R.id.radioButton_orderByFavorite -> {
+                    Log.d("aaa", "2")
+                }
+                R.id.radioButton_orderByName -> {
+                    Log.d("aaa", "3")
+                }
+            }
         }
     }
 }
