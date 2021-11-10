ef176d2e98cccb7a14e5dd3d06a0a326d123495d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-11 02:23:30 +0900

    feat: Recipe 업데이트 하는 기능 추가 #46

--- local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
@@ -3,10 +3,12 @@ package com.kdjj.local.dataSource
 import com.kdjj.data.recipe.RecipeLocalDataSource
 import com.kdjj.domain.model.Recipe
 import com.kdjj.local.dao.RecipeDao
+import com.kdjj.local.toEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class RecipeLocalDataSourceImpl(
+class RecipeLocalDataSourceImpl @Inject constructor(
 	private val recipeDao: RecipeDao
 ) : RecipeLocalDataSource {
 	
@@ -15,6 +17,16 @@ class RecipeLocalDataSourceImpl(
 			try {
 				recipeDao.deleteStepList(recipe.recipeId)
 				recipeDao.insertRecipe(recipe)
+				Result.success(true)
+			} catch (e: Exception) {
+				Result.failure(Exception(e.message))
+			}
+		}
+	
+	override suspend fun updateRecipe(recipe: Recipe): Result<Boolean> =
+		withContext(Dispatchers.IO) {
+			try {
+				recipeDao.updateRecipeMeta(recipe.toEntity())
 				Result.success(true)
 			} catch (e: Exception) {
 				Result.failure(Exception(e.message))
