e665b5faef19d59fc1195ea1d32eecb6ef2de627
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-10 23:24:10 +0900

    refactor: RecipeDao 이름 수정으로 인한 변경 #46

--- local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeLocalDataSourceImpl.kt
@@ -2,13 +2,13 @@ package com.kdjj.local.dataSource
 
 import com.kdjj.data.recipe.RecipeLocalDataSource
 import com.kdjj.domain.model.Recipe
-import com.kdjj.local.DAO.RecipeDAO
+import com.kdjj.local.dao.RecipeDao
 import com.kdjj.local.toEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class RecipeLocalDataSourceImpl(
-	private val recipeDatabase: RecipeDAO
+	private val recipeDatabase: RecipeDao
 ) : RecipeLocalDataSource {
 	
 	override suspend fun saveRecipe(recipe: Recipe): Result<Boolean> =
