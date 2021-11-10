6c9a1f760f6716df646b6f366277c4783033ae1d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-10 23:01:07 +0900

    feat(RecipeDao): Recipe Insert하는 코드 추가 #46

--- local/src/main/java/com/kdjj/local/dao/RecipeDao.kt
+++ local/src/main/java/com/kdjj/local/dao/RecipeDao.kt
@@ -1,9 +1,11 @@
 package com.kdjj.local.dao
 
 import androidx.room.*
+import com.kdjj.domain.model.Recipe
 import com.kdjj.local.model.RecipeEntity
 import com.kdjj.local.model.RecipeMetaEntity
 import com.kdjj.local.model.RecipeStepEntity
+import com.kdjj.local.toEntity
 
 @Dao
 interface RecipeDao {
@@ -13,6 +15,14 @@ interface RecipeDao {
 	
 	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	suspend fun insertRecipeStep(recipeStep: RecipeStepEntity)
+	
+	@Transaction
+	suspend fun insertRecipe(recipe: Recipe) {
+		insertRecipeMeta(recipe.toEntity())
+		recipe.stepList.forEachIndexed { idx, recipeStep ->
+			insertRecipeStep(recipeStep.toEntity(recipe.recipeId, idx + 1))
+		}
+	}
 	
 	@Transaction
 	@Query("SELECT * FROM RecipeMeta")
