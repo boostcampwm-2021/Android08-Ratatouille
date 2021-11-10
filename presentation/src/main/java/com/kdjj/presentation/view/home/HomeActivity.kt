9d4b9e651727d4550aa22422d103f142e0a24c6e
dongkey <utdhkim94@gmail.com>
2021-11-10 14:40:02 +0900

    feat: add HomeActivity #56

---
+++ presentation/src/main/java/com/kdjj/presentation/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.kdjj.presentation.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kdjj.presentation.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
