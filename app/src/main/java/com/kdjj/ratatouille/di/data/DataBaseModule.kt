d51a2927a7ae474567725c14b8775cec973796d8
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-10 15:58:28 +0900

    refactor: 데이터 베이스 생성 부분 모듈로 이동 #46

--- app/src/main/java/com/kdjj/ratatouille/di/data/DataBaseModule.kt
+++ app/src/main/java/com/kdjj/ratatouille/di/data/DataBaseModule.kt
@@ -1,12 +1,19 @@
 package com.kdjj.ratatouille.di.data
 
 import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.kdjj.local.database.RecipeDatabase
+import com.kdjj.local.model.RecipeTypeEntity
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Singleton
 
 @Module
@@ -15,8 +22,22 @@ object DataBaseModule {
 	
 	@Provides
 	@Singleton
-	fun provideRecipeDataBase(@ApplicationContext context: Context) =
-		RecipeDatabase.getInstance(context)
+	fun provideRecipeDataBase(@ApplicationContext context: Context): RecipeDatabase {
+		return Room.databaseBuilder(
+			context.applicationContext,
+			RecipeDatabase::class.java,
+			RecipeDatabase.RECIPE_DATABASE_NAME
+		).addCallback(object : RoomDatabase.Callback() {
+			override fun onCreate(db: SupportSQLiteDatabase) {
+				super.onCreate(db)
+				CoroutineScope(Dispatchers.IO).launch {
+					RecipeTypeEntity.defaultTypes.forEach { recipeType ->
+						db.execSQL("INSERT INTO RecipeType VALUES (${recipeType.recipeTypeId}, '${recipeType.title}');")
+					}
+				}
+			}
+		}).build()
+	}
 	
 	@Provides
 	@Singleton
--- local/src/main/java/com/kdjj/local/database/RecipeDatabase.kt
+++ local/src/main/java/com/kdjj/local/database/RecipeDatabase.kt
@@ -1,18 +1,12 @@
 package com.kdjj.local.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.kdjj.local.DAO.RecipeDAO
 import com.kdjj.local.DAO.RecipeListDao
 import com.kdjj.local.model.RecipeMetaEntity
 import com.kdjj.local.model.RecipeStepEntity
 import com.kdjj.local.model.RecipeTypeEntity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Database(
 	entities = [RecipeMetaEntity::class, RecipeTypeEntity::class, RecipeStepEntity::class],
@@ -26,21 +20,6 @@ abstract class RecipeDatabase : RoomDatabase() {
 	
 	companion object {
 		
-		fun getInstance(context: Context): RecipeDatabase {
-			return synchronized(RoomDatabase::class) {
-				Room.databaseBuilder(
-					context, RecipeDatabase::class.java, "RecipeDatabase.db"
-				).addCallback(object : RoomDatabase.Callback() {
-					override fun onCreate(db: SupportSQLiteDatabase) {
-						super.onCreate(db)
-						CoroutineScope(Dispatchers.IO).launch {
-							RecipeTypeEntity.defaultTypes.forEach { recipeType ->
-								getInstance(context).getRecipeDao().insertRecipeType(recipeType)
-							}
-						}
-					}
-				}).build()
-			}
-		}
+		const val RECIPE_DATABASE_NAME = "RecipeDatabase.db"
 	}
 }
