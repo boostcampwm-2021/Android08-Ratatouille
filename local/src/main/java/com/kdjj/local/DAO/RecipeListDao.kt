9c34f953e0a850d45154f0b59f429c9e2b647af6
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-10 17:13:55 +0900

    feat: 로컬 인기 레시피 리스트 local layer #41

--- local/src/main/java/com/kdjj/local/DAO/RecipeListDao.kt
+++ local/src/main/java/com/kdjj/local/DAO/RecipeListDao.kt
@@ -11,4 +11,8 @@ interface RecipeListDao {
     @Transaction
     @Query("SELECT * FROM RecipeMeta ORDER BY createTime LIMIT :pageSize OFFSET :page")
     suspend fun fetchLatestRecipeList(pageSize: Int, page: Int): List<RecipeEntity>
+
+    @Transaction
+    @Query("SELECT * FROM RecipeMeta WHERE isFavorite = :favorite ORDER BY createTime LIMIT :pageSize OFFSET :index")
+    suspend fun fetchFavoriteRecipeList(pageSize: Int, index: Int, favorite:Boolean = true): List<RecipeEntity>
 }
