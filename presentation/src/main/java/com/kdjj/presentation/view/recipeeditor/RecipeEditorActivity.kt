fb4ceedaf3f08a230d50e598cc9d08f8f1055c13
K.H.Park <svclaw2000@gmail.com>
2021-11-11 13:47:28 +0900

    feat: get image from gallery #49

--- presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipeeditor/RecipeEditorActivity.kt
@@ -1,11 +1,15 @@
 package com.kdjj.presentation.view.recipeeditor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
 import com.kdjj.presentation.databinding.ActivityRecipeEditorBinding
+import com.kdjj.presentation.model.RecipeEditorItem
 import com.kdjj.presentation.view.adapter.RecipeEditorListAdapter
 import com.kdjj.presentation.viewmodel.recipeeditor.RecipeEditorViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +18,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class RecipeEditorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeEditorBinding
     private val viewModel: RecipeEditorViewModel by viewModels()
+
+    private val galleryLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            viewModel.setImage(result?.data?.dataString)
+        } else {
+            viewModel.setImage(null)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +41,36 @@ class RecipeEditorActivity : AppCompatActivity() {
 
         val adapter = RecipeEditorListAdapter(viewModel)
         binding.recyclerViewEditor.adapter = adapter
+
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.liveImgTarget.observe(this) { model ->
+            model?.let { showSelectImageDialog() }
+        }
+    }
+
+    private fun showSelectImageDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.selectPhoto)
+            .setItems(R.array.photoSourceSelection) { _, i ->
+                when (i) {
+                    0 -> {
+                    }
+                    1 -> {
+                        galleryLauncher.launch(
+                            Intent().apply {
+                                type = "image/*"
+                                action = Intent.ACTION_GET_CONTENT
+                            }
+                        )
+                    }
+                    2 -> {
+                        viewModel.setImageEmpty()
+                    }
+                }
+            }
+            .show()
     }
 }
