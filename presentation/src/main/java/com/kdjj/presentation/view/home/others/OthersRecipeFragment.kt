4f62bed0b2875ed7c9b621d5237a64037e47ad52
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-11 13:53:26 +0900

    feat: 힐끔보기 화면 레시피 목록 리사이클러뷰 UI #60

--- presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
+import com.kdjj.presentation.databinding.FragmentOthersRecipeBinding
 
 class OthersRecipeFragment : Fragment() {
+
+    private var _binding: FragmentOthersRecipeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_others_recipe, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_others_recipe,
+            container,
+            false
+        )
+        return binding.root
     }
 }
