24ee06590c68b8a8a8a6297cc972bc3bee505bd3
dongkey <utdhkim94@gmail.com>
2021-11-13 01:29:00 +0900

    feat: radio button에 의한 분기처리 구현 #87

---
+++ presentation/src/main/java/com/kdjj/presentation/view/home/my/SortType.kt
@@ -0,0 +1,7 @@
+package com.kdjj.presentation.view.home.my
+
+enum class SortType {
+    SORT_BY_TIME,
+    SORT_BY_FAVORITE,
+    SORT_BY_NAME
+}
--- presentation/src/main/java/com/kdjj/presentation/viewmodel/my/MyRecipeViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/my/MyRecipeViewModel.kt
@@ -1,8 +1,29 @@
 package com.kdjj.presentation.viewmodel.my
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kdjj.presentation.view.home.my.SortType
 import javax.inject.Inject
 
 internal class MyRecipeViewModel @Inject constructor(
 ) : ViewModel() {
+
+    private val _liveSortType = MutableLiveData<SortType>(SortType.SORT_BY_TIME)
+    val liveSortType: LiveData<SortType> get() = _liveSortType
+
+    fun fetchLocalLatestRecipeList(){
+        Log.d("aaa", "fetchLocalLatest")
+    }
+
+    fun fetchLocalFavoriteRecipeList(){
+        Log.d("aaa", "fetchLocalFavorite")
+    }
+
+    fun setSortType(sortType: SortType){
+        if(_liveSortType.value != sortType){
+            _liveSortType.value = sortType
+        }
+    }
 }
