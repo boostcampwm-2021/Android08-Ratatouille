f30faaa9d9b89d5392c670d05225f618a40f89ee
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-15 07:39:14 +0900

    feat: remote 레시피 업데이트 UseCase 구현 #76

---
+++ domain/src/main/java/com/kdjj/domain/usecase/UpdateRemoteRecipeUseCase.kt
@@ -0,0 +1,15 @@
+package com.kdjj.domain.usecase
+
+import com.kdjj.domain.repository.RecipeRepository
+import com.kdjj.domain.request.UpdateRemoteRecipeRequest
+import javax.inject.Inject
+
+class UpdateRemoteRecipeUseCase @Inject constructor(
+    private val recipeRepository: RecipeRepository
+) : UseCase<UpdateRemoteRecipeRequest, Unit> {
+    
+    override suspend fun invoke(request: UpdateRemoteRecipeRequest): Result<Unit> {
+        // TODO : workmanager를 이용한 백그라운드 처리
+        return recipeRepository.uploadRecipe(request.recipe)
+    }
+}
