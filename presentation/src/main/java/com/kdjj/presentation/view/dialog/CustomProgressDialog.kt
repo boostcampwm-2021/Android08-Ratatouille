8cac194743277b0fb30f34fe2236e0c3879cb138
K.H.Park <svclaw2000@gmail.com>
2021-11-14 23:01:23 +0900

    feat: add custom progress dialog #74

---
+++ presentation/src/main/java/com/kdjj/presentation/view/dialog/CustomProgressDialog.kt
@@ -0,0 +1,16 @@
+package com.kdjj.presentation.view.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import com.kdjj.presentation.R
+
+class CustomProgressDialog(context: Context) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_progress)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+}
