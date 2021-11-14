9803364c100c3f264c1abec60fc4ac4474a94828
K.H.Park <svclaw2000@gmail.com>
2021-11-14 17:31:35 +0900

    refactor: separate binding adapter by view #74

---
+++ presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -0,0 +1,11 @@
+package com.kdjj.presentation.view.bindingadapter
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+@Suppress("UNCHECKED_CAST")
+@BindingAdapter("app:submitList")
+fun <T, VH : RecyclerView.ViewHolder> RecyclerView.submitList(list: List<T>?) {
+    (adapter as? ListAdapter<T, VH>)?.submitList(list)
+}
