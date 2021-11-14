ab8b9aa175df87611295c6f34d1a554d586ef326
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-15 07:22:51 +0900

    feat: 레시피 업로드 UseCase 구현 #76

---
+++ domain/src/main/java/com/kdjj/domain/usecase/UploadRecipeUseCase.kt
@@ -0,0 +1,14 @@
+package com.kdjj.domain.usecase
+
+import com.kdjj.domain.repository.RecipeRepository
+import com.kdjj.domain.request.UploadRecipeRequest
+import javax.inject.Inject
+
+class UploadRecipeUseCase @Inject constructor(
+    private val recipeRepository: RecipeRepository
+) : UseCase<UploadRecipeRequest, Unit> {
+    
+    override suspend fun invoke(request: UploadRecipeRequest): Result<Unit> {
+        return recipeRepository.uploadRecipe(request.recipe)
+    }
+}
