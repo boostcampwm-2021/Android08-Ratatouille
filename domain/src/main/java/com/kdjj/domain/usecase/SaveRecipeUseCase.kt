f7036a433f89699e569e8ba0050239836c2cc723
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-14 18:29:08 +0900

    feat(SaveRecipeUseCase): 레시피 이미지 저장하는 로직 추가 close #73

--- domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeUseCase.kt
@@ -1,13 +1,48 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.repository.RecipeImageRepository
 import com.kdjj.domain.repository.RecipeRepository
 import com.kdjj.domain.request.SaveRecipeRequest
 import javax.inject.Inject
 
 internal class SaveRecipeUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+    private val recipeRepository: RecipeRepository,
+    private val imageRepository: RecipeImageRepository
 ) : UseCase<SaveRecipeRequest, Boolean> {
     
     override suspend fun invoke(request: SaveRecipeRequest): Result<Boolean> =
-        recipeRepository.saveRecipe(request.recipe)
+        kotlin.runCatching {
+            val recipe = request.recipe
+            val recipeImageUri = when (recipe.imgPath.isNotEmpty()) {
+                true -> {
+                    val recipeImageByteArray =
+                        imageRepository.convertLocalUriToByteArray(recipe.imgPath).getOrThrow()
+                    imageRepository.convertByteArrayToLocalStorageUri(
+                        recipeImageByteArray, recipe.recipeId
+                    ).getOrThrow()
+                }
+                false -> ""
+            }
+            val recipeStepList = recipe.stepList.map { step ->
+                val stepImageUri = when (step.imgPath.isNotEmpty()) {
+                    true -> {
+                        val stepImageByteArray =
+                            imageRepository.convertLocalUriToByteArray(step.imgPath).getOrThrow()
+                        imageRepository.convertByteArrayToLocalStorageUri(
+                            stepImageByteArray, step.stepId
+                        ).getOrThrow()
+                    }
+                    false -> ""
+                }
+                step.copy(imgPath = stepImageUri)
+            }
+            
+            recipeRepository.saveRecipe(
+                recipe.copy(
+                    imgPath = recipeImageUri,
+                    stepList = recipeStepList
+                )
+            )
+            true
+        }
 }
