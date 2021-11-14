6bdfffa10ec474c4e7db694486c635e7a7143966
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-14 16:02:59 +0900

    refactor: 함수명 명확하게 변경 #71

--- data/src/main/java/com/kdjj/data/repository/RecipeImageRepositoryImpl.kt
+++ data/src/main/java/com/kdjj/data/repository/RecipeImageRepositoryImpl.kt
@@ -6,23 +6,23 @@ import com.kdjj.domain.repository.RecipeImageRepository
 import javax.inject.Inject
 
 internal class RecipeImageRepositoryImpl @Inject constructor(
-	private val recipeImageLocalDataSource: RecipeImageLocalDataSource,
-	private val recipeImageRemoteDataSource: RecipeImageRemoteDataSource
-): RecipeImageRepository {
-
-    override suspend fun convertToByteArrayRemote(uri: String): Result<ByteArray> {
+    private val recipeImageLocalDataSource: RecipeImageLocalDataSource,
+    private val recipeImageRemoteDataSource: RecipeImageRemoteDataSource
+) : RecipeImageRepository {
+    
+    override suspend fun convertRemoteUriToByteArray(uri: String): Result<ByteArray> {
         return recipeImageRemoteDataSource.fetchRecipeImage(uri)
     }
-
-    override suspend fun convertToByteArrayLocal(uri: String): Result<ByteArray> {
+    
+    override suspend fun convertLocalUriToByteArray(uri: String): Result<ByteArray> {
         return recipeImageLocalDataSource.convertToByteArray(uri)
     }
-
-    override suspend fun convertToRemoteStorageUri(uri: String): Result<String> {
+    
+    override suspend fun convertLocalUriToRemoteStorageUri(uri: String): Result<String> {
         return recipeImageRemoteDataSource.uploadRecipeImage(uri)
     }
-
-    override suspend fun convertToLocalStorageUri(
+    
+    override suspend fun convertByteArrayToLocalStorageUri(
         byteArray: ByteArray,
         fileName: String
     ): Result<String> {
