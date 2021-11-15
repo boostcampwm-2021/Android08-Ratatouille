7f21a5468fa41ec8c5c4e2af431707ab618f54e2
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-15 19:27:56 +0900

    feat: 힐끔보기 화면 인기순, 최신순 필터링 viewmodel logic #83

--- presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
@@ -1,7 +1,6 @@
 package com.kdjj.presentation.view.home.others
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -57,7 +56,6 @@ class OthersRecipeFragment : Fragment() {
     }
 
     private fun setAdapter() {
-        Log.d("Test", System.currentTimeMillis().toString())
         val adapter = OthersRecipeListAdapter()
         binding.recyclerViewOthersRecipe.adapter = adapter
         adapter.submitList(
--- presentation/src/main/java/com/kdjj/presentation/viewmodel/others/OthersViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/others/OthersViewModel.kt
@@ -1,18 +1,123 @@
 package com.kdjj.presentation.viewmodel.others
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.request.FetchRemoteLatestRecipeListRequest
+import com.kdjj.domain.model.request.FetchRemotePopularRecipeListRequest
+import com.kdjj.domain.usecase.UseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OthersViewModel @Inject constructor(
+    private val fetchRemoteLatestRecipeListUseCase: UseCase<FetchRemoteLatestRecipeListRequest, List<Recipe>>,
+    private val fetchRemotePopularRecipeListUseCase: UseCase<FetchRemotePopularRecipeListRequest, List<Recipe>>,
 ) : ViewModel() {
 
-    fun fetchRemoteLatestRecipeList() {
-        // todo : fetchRemoteLatestRecipeList
+    private var _liveSortType = MutableLiveData<OthersSortType>()
+    val liveSortType: LiveData<OthersSortType> get() = _liveSortType
+
+    private var _liveFetchLock = MutableLiveData(false)
+    val liveFetchLock: LiveData<Boolean> get() = _liveFetchLock
+
+    private var _liveRecipeList = MutableLiveData<List<Recipe>>()
+    val liveRecipeList: LiveData<List<Recipe>> get() = _liveRecipeList
+
+    private var fetchingJob: Job? = null
+
+    val setFetchEnabled = Runnable {
+        _liveFetchLock.value = false
     }
 
-    fun fetchRemotePopularRecipeList() {
-        // todo : fetchRemotePopularRecipeList
+    init {
+        Log.d("Test", "OthersViewModel init")
+        setChecked(OthersSortType.LATEST)
+    }
+
+    fun setChecked(othersSortType: OthersSortType) {
+        if (_liveSortType.value != othersSortType) {
+            Log.d("Test", "OthersViewModel setChecked")
+            // sort type 바뀔 때마다 리스트 초기화 후, 바뀐 type 으로 받아오기
+            _liveSortType.value = othersSortType
+            initFetching()
+            fetchNextRecipeListPage()
+        }
+    }
+
+    // 리스트 Fetch 관련 모든 설정을 초기화
+    private fun initFetching() {
+        fetchingJob?.cancel()
+        _liveFetchLock.value = false
+        _liveRecipeList.value = listOf()
+    }
+
+    fun fetchNextRecipeListPage() {
+        if (liveFetchLock.value == true) return
+
+        _liveSortType.value?.let {
+
+            _liveFetchLock.value = true
+
+            fetchingJob = viewModelScope.launch {
+                if (it == OthersSortType.LATEST) {
+                    fetchRemoteLatestRecipeList()
+                } else {
+                    fetchRemotePopularRecipeList()
+                }
+            }
+        }
+    }
+
+    private suspend fun fetchRemoteLatestRecipeList() {
+        val lastVisibleCreateTime = _liveRecipeList.value?.let {
+            if (it.isEmpty()) Long.MAX_VALUE
+            else it.last().createTime
+        } ?: Long.MAX_VALUE
+
+        onRecipeListFetched(
+            fetchRemoteLatestRecipeListUseCase(
+                FetchRemoteLatestRecipeListRequest(lastVisibleCreateTime)
+            )
+        )
+    }
+
+    private suspend fun fetchRemotePopularRecipeList() {
+        val lastVisibleViewCount = _liveRecipeList.value?.let {
+            if (it.isEmpty()) Int.MAX_VALUE
+            else it.last().viewCount
+        } ?: Int.MAX_VALUE
+
+        onRecipeListFetched(
+            fetchRemotePopularRecipeListUseCase(
+                FetchRemotePopularRecipeListRequest(lastVisibleViewCount)
+            )
+        )
+    }
+
+    private fun onRecipeListFetched(result: Result<List<Recipe>>) {
+        result.onSuccess { list ->
+            _liveRecipeList.value?.plus(list) ?: run {
+                _liveRecipeList.value = list
+            }
+        }.onFailure {
+            // view 에게 알리기
+            _liveFetchLock.value = false
+        }
+    }
+
+    override fun onCleared() {
+        Log.d("Test", "OthersViewModel onCleared")
+        super.onCleared()
+    }
+
+    enum class OthersSortType {
+        POPULAR, LATEST
     }
 }
