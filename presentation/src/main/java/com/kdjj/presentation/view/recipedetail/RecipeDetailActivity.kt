1cebddb47b5d9e6ea1b6624db43b289ea4bb8a2b
K.H.Park <svclaw2000@gmail.com>
2021-11-15 20:17:27 +0900

    feat: add step list to detail activity #85

--- presentation/src/main/java/com/kdjj/presentation/view/recipedetail/RecipeDetailActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipedetail/RecipeDetailActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.kdjj.domain.model.*
 import com.kdjj.presentation.R
 import com.kdjj.presentation.databinding.ActivityRecipeDetailBinding
+import com.kdjj.presentation.view.adapter.RecipeDetailStepListAdapter
 import com.kdjj.presentation.viewmodel.recipedetail.RecipeDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,8 +18,34 @@ class RecipeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeDetailBinding
     private val viewModel: RecipeDetailViewModel by viewModels()
 
+    private lateinit var stepListAdapter: RecipeDetailStepListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_detail)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        stepListAdapter = RecipeDetailStepListAdapter(viewModel)
+        binding.recyclerViewDetailStep.adapter = stepListAdapter
+
+        viewModel.initializeWith(Recipe(
+            "",
+            "",
+            RecipeType(1, "기타"),
+            "",
+            "",
+            listOf(
+                RecipeStep("", "단계1", RecipeStepType.PREPARE, "1단계입니다.", "", 0),
+                RecipeStep("", "단계2", RecipeStepType.COOK, "2단계입니다. 약 20초 걸립니다.", "", 20),
+                RecipeStep("", "단계3", RecipeStepType.COOK, "3단계입니다. 약 2분 30초 걸립니다.", "", 150),
+                RecipeStep("", "단계4", RecipeStepType.COOK, "4단계입니다. 약 10초 걸립니다.", "", 10),
+            ),
+            "",
+            0,
+            false,
+            0,
+            RecipeState.CREATE
+        ))
     }
 }
--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipedetail/RecipeDetailViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipedetail/RecipeDetailViewModel.kt
@@ -1,6 +1,10 @@
 package com.kdjj.presentation.viewmodel.recipedetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.RecipeStep
 import javax.inject.Inject
 
 
@@ -8,4 +12,10 @@ class RecipeDetailViewModel @Inject constructor(
     // TODO("fetchLocalRecipeById")
 ) : ViewModel() {
 
+    private val _liveStepList = MutableLiveData<List<RecipeStep>>()
+    val liveStepList: LiveData<List<RecipeStep>> get() = _liveStepList
+
+    fun initializeWith(recipe: Recipe) {
+        _liveStepList.value = recipe.stepList
+    }
 }
