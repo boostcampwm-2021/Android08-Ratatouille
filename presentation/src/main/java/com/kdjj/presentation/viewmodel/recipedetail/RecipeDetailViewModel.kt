c056598131684350a306cef429c3d3519adbc55b
K.H.Park <svclaw2000@gmail.com>
2021-11-16 00:17:15 +0900

    feat: add timer information to detail activity #85

--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipedetail/RecipeDetailViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipedetail/RecipeDetailViewModel.kt
@@ -3,6 +3,7 @@ package com.kdjj.presentation.viewmodel.recipedetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.kdjj.domain.model.Recipe
 import com.kdjj.domain.model.RecipeStep
 import javax.inject.Inject
@@ -15,7 +16,11 @@ class RecipeDetailViewModel @Inject constructor(
     private val _liveStepList = MutableLiveData<List<RecipeStep>>()
     val liveStepList: LiveData<List<RecipeStep>> get() = _liveStepList
 
+    private val _liveSelectedStep = MutableLiveData<RecipeStep>()
+    val liveSelectedStep: LiveData<RecipeStep> get() = _liveSelectedStep
+
     fun initializeWith(recipe: Recipe) {
         _liveStepList.value = recipe.stepList
+        _liveSelectedStep.value = recipe.stepList[0]
     }
 }
