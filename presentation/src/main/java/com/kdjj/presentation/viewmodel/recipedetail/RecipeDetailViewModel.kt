93aa93bcfe9ab2a16933db3576a0ed827a6eac60
K.H.Park <svclaw2000@gmail.com>
2021-11-15 18:23:30 +0900

    feat: initialize DetailViewModel #85

---
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipedetail/RecipeDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.kdjj.presentation.viewmodel.recipedetail
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+
+class RecipeDetailViewModel @Inject constructor(
+    // TODO("fetchLocalRecipeById")
+) : ViewModel() {
+
+}
