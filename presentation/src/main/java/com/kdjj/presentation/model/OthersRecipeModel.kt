a5d5f3300d3b8cd63248411e0c837e12dc74a2f4
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-17 00:49:43 +0900

    feature: OthersRecipeModel.kt 추가 #83

---
+++ presentation/src/main/java/com/kdjj/presentation/model/OthersRecipeModel.kt
@@ -0,0 +1,11 @@
+package com.kdjj.presentation.model
+
+data class OthersRecipeModel(
+    val recipeId: String,
+    val title: String,
+    val totalTime: String,
+    val stuff: String,
+    val updateTime: String,
+    val viewCount: Int,
+    val imgPath: String,
+)
