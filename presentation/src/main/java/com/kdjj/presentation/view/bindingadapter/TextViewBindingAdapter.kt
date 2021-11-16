0a8b817fbf0d4292bb5b7ae8e96cf40fc6455985
dongkey <utdhkim94@gmail.com>
2021-11-16 23:39:55 +0900

    refactor: 리뷰반영 2 #87

--- presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
@@ -14,13 +14,3 @@ fun TextView.setTotalTimeText(recipe: Recipe?) {
     val totalTime = calculateTotalTime(recipe ?: return)
     this.text = "소요시간: $totalTime"
 }
-
-@BindingAdapter("animatedText")
-fun TextView.setAnimatedText(isAnimate: Boolean) {
-    if(isAnimate){
-        ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, 100f, 0f).apply {
-            duration = 200
-            start()
-        }
-    }
-}
