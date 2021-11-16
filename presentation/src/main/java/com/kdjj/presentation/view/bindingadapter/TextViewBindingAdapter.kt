38aece18a2f5d4fa2d355f25a942e2c157e9d6b2
dongkey <utdhkim94@gmail.com>
2021-11-16 22:15:58 +0900

    feat: Recipe 클릭시 텍스트 뷰 에니메이션 추가 close #87

--- presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
@@ -1,6 +1,9 @@
 package com.kdjj.presentation.view.bindingadapter
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.kdjj.domain.model.Recipe
@@ -8,7 +11,16 @@ import com.kdjj.presentation.common.calculateTotalTime
 
 @BindingAdapter("calculateTotalTime")
 fun TextView.setTotalTimeText(recipe: Recipe?) {
-
-    val totalTime = calculateTotalTime(recipe?: return)
+    val totalTime = calculateTotalTime(recipe ?: return)
     this.text = "소요시간: $totalTime"
+}
+
+@BindingAdapter("animatedText")
+fun TextView.setAnimatedText(isAnimate: Boolean) {
+    if(isAnimate){
+        ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, 100f, 0f).apply {
+            duration = 200
+            start()
+        }
+    }
 }
