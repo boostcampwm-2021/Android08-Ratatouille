9ff3dafc6c507e1818573ee3381db62a232b38ce
dongkey <utdhkim94@gmail.com>
2021-11-17 23:15:52 +0900

    refactor: 내 레시피 소요시간 표시 수정 #105

--- presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/TextViewBindingAdapter.kt
@@ -2,7 +2,6 @@ package com.kdjj.presentation.view.bindingadapter
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.kdjj.domain.model.Recipe
 import com.kdjj.presentation.R
 
 @BindingAdapter("formatTotalTime")
