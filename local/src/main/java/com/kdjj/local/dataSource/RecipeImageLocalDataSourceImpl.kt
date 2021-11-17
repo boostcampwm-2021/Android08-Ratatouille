c6cd859e8f9c4392fcdc795da5d5062aee5d0444
dongkey <utdhkim94@gmail.com>
2021-11-17 16:42:52 +0900

    refactor: RecipeImageLocalDataSource convertToInternalStorageUri 함수 수정  #102

--- local/src/main/java/com/kdjj/local/dataSource/RecipeImageLocalDataSourceImpl.kt
+++ local/src/main/java/com/kdjj/local/dataSource/RecipeImageLocalDataSourceImpl.kt
@@ -1,12 +1,7 @@
 package com.kdjj.local.dataSource
 
-import android.media.ExifInterface
-import com.kdjj.data.common.errorMap
 import com.kdjj.data.datasource.RecipeImageLocalDataSource
 import com.kdjj.local.FileSaveHelper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.IOException
 import javax.inject.Inject
 
 internal class RecipeImageLocalDataSourceImpl @Inject constructor(
@@ -15,34 +10,23 @@ internal class RecipeImageLocalDataSourceImpl @Inject constructor(
 
     override suspend fun convertToByteArray(
         uri: String
-    ): Result<ByteArray> {
+    ): Result<Pair<ByteArray, Float?>> {
         return fileSaveHelper.convertToByteArray(uri)
     }
 
     override suspend fun convertToInternalStorageUri(
         byteArray: ByteArray,
-        fileName: String
+        fileName: String,
+        degree: Float?
     ): Result<String> {
-        return fileSaveHelper.convertToInternalStorageUri(byteArray, fileName)
+        return fileSaveHelper.convertToInternalStorageUri(byteArray, fileName, degree)
     }
 
     override suspend fun copyExif(
         oldPath: String,
         newPath: String
-    ): Result<Unit> =
-        withContext(Dispatchers.IO) {
-            kotlin.runCatching {
-                val oldExif = ExifInterface(oldPath)
-                val exifOrientation = oldExif.getAttribute(ExifInterface.TAG_ORIENTATION)
-                exifOrientation?.let {
-                    val newExif = ExifInterface(newPath)
-                    newExif.setAttribute(ExifInterface.TAG_ORIENTATION, exifOrientation)
-                    newExif.saveAttributes()
-                }
-                return@runCatching
-            }.errorMap {
-                Exception(it.message)
-            }
-        }
+    ): Result<Unit> {
+        return fileSaveHelper.copyExif(oldPath, newPath)
+    }
 }
 
