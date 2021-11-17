aecd84f8aa79e731378a580bb56f4be7b32986e2
dongkey <utdhkim94@gmail.com>
2021-11-17 16:39:56 +0900

    refactor: copyExif 함수 FileSaveHelper.kt 이동  #102

--- local/src/main/java/com/kdjj/local/FileSaveHelper.kt
+++ local/src/main/java/com/kdjj/local/FileSaveHelper.kt
@@ -3,7 +3,10 @@ package com.kdjj.local
 import android.content.ContentResolver
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
+import android.os.Build
 import android.os.FileUtils
 import android.util.Log
 import com.kdjj.data.common.errorMap
@@ -21,10 +24,24 @@ class FileSaveHelper @Inject constructor(
     private val contentResolver: ContentResolver
 ) {
 
-    suspend fun convertToByteArray(uri: String): Result<ByteArray> = withContext(Dispatchers.IO) {
+    suspend fun convertToByteArray(uri: String): Result<Pair<ByteArray, Float?>> = withContext(Dispatchers.IO) {
         runCatching {
             val inputStream = contentResolver.openInputStream(Uri.parse(uri))
-            inputStream?.readBytes() ?: throw Exception()
+            val byteArray = inputStream?.readBytes() ?: throw Exception()
+
+            val exifInputStream = contentResolver.openInputStream(Uri.parse(uri))
+            val oldExif = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                ExifInterface(exifInputStream ?: throw Exception())
+            } else {
+                ExifInterface(uri)
+            }
+            val degree = when (oldExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> 90f
+                ExifInterface.ORIENTATION_ROTATE_180 -> 180f
+                ExifInterface.ORIENTATION_ROTATE_270 -> 270f
+                else -> null
+            }
+            byteArray to degree
         }.errorMap {
             Exception(it.message)
         }
@@ -32,13 +49,14 @@ class FileSaveHelper @Inject constructor(
 
     suspend fun convertToInternalStorageUri(
         byteArray: ByteArray,
-        fileName: String
+        fileName: String,
+        degree: Float?
     ): Result<String> = withContext(Dispatchers.IO) {
         var fos: FileOutputStream? = null
         runCatching {
             val filePath = "$fileDir/${fileName}.png"
             fos = FileOutputStream(filePath)
-            val bitmap = convertByteArrayToBitmap(byteArray)
+            val bitmap = convertByteArrayToBitmap(byteArray, degree)
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
             filePath
         }.errorMap {
@@ -46,7 +64,7 @@ class FileSaveHelper @Inject constructor(
         }
     }
 
-    private fun convertByteArrayToBitmap(byteArray: ByteArray): Bitmap {
+    private fun convertByteArrayToBitmap(byteArray: ByteArray, degree: Float?): Bitmap {
         val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
         val width = bitmap.width.toFloat()
         val height = bitmap.height.toFloat()
@@ -59,8 +77,37 @@ class FileSaveHelper @Inject constructor(
         } else {
             width to height
         }
-        return Bitmap.createScaledBitmap(bitmap, scaledWidth.toInt(), scaledHeight.toInt(), true)
+        return Bitmap.createScaledBitmap(bitmap, scaledWidth.toInt(), scaledHeight.toInt(), true).let { scaledBitmap ->
+            degree?.let {
+                val matrix = Matrix().apply {
+                    postRotate(degree)
+                }
+                Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.width, scaledBitmap.height, matrix, true)
+            } ?: scaledBitmap
+        }
     }
+
+    suspend fun copyExif(
+        oldPath: String,
+        newPath: String
+    ): Result<Unit> =
+        withContext(Dispatchers.IO) {
+            runCatching {
+                val inputStream = contentResolver.openInputStream(Uri.parse(oldPath))
+                val oldExif = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    ExifInterface(inputStream ?: throw Exception())
+                } else {
+                    ExifInterface(oldPath)
+                }
+                val exifOrientation = oldExif.getAttribute(ExifInterface.TAG_ORIENTATION)
+                exifOrientation?.let {
+                    val newExif = ExifInterface(newPath)
+                    newExif.setAttribute(ExifInterface.TAG_ORIENTATION, exifOrientation)
+                    newExif.saveAttributes()
+                }
+                return@runCatching
+            }
+        }
 
     companion object{
         const val MAX_WIDTH_HEIGHT_SIZE = 300
