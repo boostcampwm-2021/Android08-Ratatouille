baa03661d0e79a5875a1452eca0c92a72d0395c5
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-17 22:07:20 +0900

    refactor: 레시피 플로우 가져오는 UseCase 이름 변경 #103

--- domain/src/main/java/com/kdjj/domain/usecase/GetLocalRecipeFlowUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/GetLocalRecipeFlowUseCase.kt
@@ -1,15 +1,15 @@
 package com.kdjj.domain.usecase
 
 import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.request.GetLocalRecipeRequest
+import com.kdjj.domain.model.request.GetLocalRecipeFlowRequest
 import com.kdjj.domain.repository.RecipeRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetLocalRecipeUseCase @Inject constructor(
+class GetLocalRecipeFlowUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
-) : UseCase<GetLocalRecipeRequest, Flow<Recipe>> {
+) : UseCase<GetLocalRecipeFlowRequest, Flow<Recipe>> {
     
-    override suspend fun invoke(request: GetLocalRecipeRequest): Result<Flow<Recipe>> =
+    override suspend fun invoke(request: GetLocalRecipeFlowRequest): Result<Flow<Recipe>> =
         recipeRepository.getLocalRecipeFlow(request.recipeId)
 }
