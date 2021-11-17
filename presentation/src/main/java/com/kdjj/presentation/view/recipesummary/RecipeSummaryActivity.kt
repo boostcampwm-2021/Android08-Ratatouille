7cad139a7b62b2e9550275546ff558b0a7fd58ec
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-18 02:18:49 +0900

    feat(RecipeSummary): ViewModel 초기화 함수 구현 #111

--- presentation/src/main/java/com/kdjj/presentation/view/recipesummary/RecipeSummaryActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipesummary/RecipeSummaryActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.kdjj.domain.model.RecipeState
 import com.kdjj.presentation.R
+import com.kdjj.presentation.common.EventObserver
+import com.kdjj.presentation.common.RECIPE_ID
+import com.kdjj.presentation.common.RECIPE_STATE
 import com.kdjj.presentation.databinding.ActivityRecipeSummaryBinding
+import com.kdjj.presentation.view.dialog.ConfirmDialogBuilder
 import com.kdjj.presentation.viewmodel.recipesummary.RecipeSummaryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,5 +28,32 @@ class RecipeSummaryActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         
         setSupportActionBar(binding.toolbarSummary)
+        
+        initViewModel()
+        initObserver()
+    }
+    
+    private fun initObserver() = with(recipeSummaryViewModel){
+        eventNoInfo.observe(this@RecipeSummaryActivity, EventObserver{
+            showNoInfoDialog()
+        })
+    }
+    
+    private fun initViewModel() {
+        intent.extras?.let{ bundle ->
+            val recipeId = bundle.getString(RECIPE_ID)
+            val recipeState = bundle.getSerializable(RECIPE_STATE) as? RecipeState
+            recipeSummaryViewModel.initViewModel(recipeId, recipeState)
+        }
+    }
+    
+    private fun showNoInfoDialog(){
+        ConfirmDialogBuilder.create(
+            this,
+            "정보 에러",
+            "전달되지 않은 정보가 존재합니다.\n확인을 눌러 이전화면으로 돌아갑니다."
+        ){
+            finish()
+        }
     }
 }
--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipesummary/RecipeSummaryViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipesummary/RecipeSummaryViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.RecipeType
-import com.kdjj.domain.model.request.EmptyRequest
+import com.kdjj.domain.model.RecipeState
 import com.kdjj.domain.model.request.GetLocalRecipeFlowRequest
 import com.kdjj.domain.usecase.UseCase
+import com.kdjj.presentation.common.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
@@ -22,18 +22,35 @@ class RecipeSummaryViewModel @Inject constructor(
     
     private val _liveRecipe = MutableLiveData<Recipe>()
     val liveRecipe: LiveData<Recipe> = _liveRecipe
+    private val _eventNoInfo = MutableLiveData<Event<Unit>>()
+    val eventNoInfo: LiveData<Event<Unit>> = _eventNoInfo
     private var isInitialized = false
     
-    fun initViewModel(recipeId: String) {
+    fun initViewModel(recipeId: String?, recipeState: RecipeState?) {
         if (isInitialized) return
-        viewModelScope.launch {
-            getRecipeFlowUseCase(GetLocalRecipeFlowRequest(recipeId))
-                .onSuccess { recipeFlow ->
-                    recipeFlow.collect { recipe ->
-                        _liveRecipe.value = recipe
+        if (recipeId == null || recipeState == null) notifyNoInfo()
+        else {
+            viewModelScope.launch {
+                when (recipeState) {
+                    RecipeState.CREATE, RecipeState.UPLOAD -> {
+                        getRecipeFlowUseCase(GetLocalRecipeFlowRequest(recipeId))
+                            .onSuccess { recipeFlow ->
+                                recipeFlow.collect { recipe ->
+                                    _liveRecipe.value = recipe
+                                }
+                            }
+                    }
+                    RecipeState.DOWNLOAD -> {
+                        // TODO : Remote에서 레시피 가져오는 기능
                     }
                 }
+            }
         }
+        
         isInitialized = true
+    }
+    
+    private fun notifyNoInfo() {
+        _eventNoInfo.value = Event(Unit)
     }
 }
