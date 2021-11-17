a7cbea2553a64fdd4bfbe3f8be5193149bcc29e7
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-17 16:29:00 +0900

    feat: 레시피 개요 화면 초기 생성 #103

---
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipesummary/RecipeSummaryViewModel.kt
@@ -0,0 +1,8 @@
+package com.kdjj.presentation.viewmodel.recipesummary
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class RecipeSummaryViewModel: ViewModel() {
+}
