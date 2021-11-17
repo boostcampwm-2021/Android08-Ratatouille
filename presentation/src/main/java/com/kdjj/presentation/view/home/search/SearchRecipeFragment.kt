1e53ae3b53514e309039a2151b598a694eda8a0d
K.H.Park <svclaw2000@gmail.com>
2021-11-18 00:29:13 +0900

    feat: add search toolbar #107

--- presentation/src/main/java/com/kdjj/presentation/view/home/search/SearchRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/search/SearchRecipeFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
+import com.kdjj.presentation.databinding.FragmentSearchRecipeBinding
 
 class SearchRecipeFragment : Fragment() {
+
+    private var _binding: FragmentSearchRecipeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search_recipe, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_recipe, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editTextSearch.requestFocus()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
