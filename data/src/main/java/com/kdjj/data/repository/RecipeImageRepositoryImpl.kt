fc46c377659599be527b2c4eca1dd652fc154268
dongkey <utdhkim94@gmail.com>
2021-11-17 16:44:50 +0900

    refactor: RecipeImageRepository RecipeImageLocalDataSource 수정사항 반영  #102

--- data/src/main/java/com/kdjj/data/repository/RecipeImageRepositoryImpl.kt
+++ data/src/main/java/com/kdjj/data/repository/RecipeImageRepositoryImpl.kt
@@ -9,29 +9,34 @@ internal class RecipeImageRepositoryImpl @Inject constructor(
     private val recipeImageLocalDataSource: RecipeImageLocalDataSource,
     private val recipeImageRemoteDataSource: RecipeImageRemoteDataSource
 ) : RecipeImageRepository {
-    
-    override suspend fun convertRemoteUriToByteArray(
-        uri: String
-    ): Result<ByteArray> {
-        return recipeImageRemoteDataSource.fetchRecipeImage(uri)
-    }
-    
-    override suspend fun convertLocalUriToByteArray(
-        uri: String
-    ): Result<ByteArray> {
-        return recipeImageLocalDataSource.convertToByteArray(uri)
-    }
-    
-    override suspend fun convertLocalUriToRemoteStorageUri(
+
+    override suspend fun convertInternalUriToRemoteStorageUri(
         uri: String
     ): Result<String> {
         return recipeImageRemoteDataSource.uploadRecipeImage(uri)
     }
-    
-    override suspend fun convertByteArrayToLocalStorageUri(
-        byteArray: ByteArray,
+
+    override suspend fun copyExternalImageToInternal(
+        uri: String,
         fileName: String
-    ): Result<String> {
-        return recipeImageLocalDataSource.convertToInternalStorageUri(byteArray, fileName)
+    ): Result<String> = runCatching {
+        val (byteArray, degree) = recipeImageLocalDataSource.convertToByteArray(uri)
+            .getOrThrow()
+        val newUri = recipeImageLocalDataSource.convertToInternalStorageUri(byteArray, fileName, degree)
+            .getOrThrow()
+        newUri
+    }
+
+    override suspend fun copyRemoteImageToInternal(
+        uri: String,
+        fileName: String
+    ): Result<String> = runCatching {
+        val byteArray = recipeImageRemoteDataSource.fetchRecipeImage(uri)
+            .getOrThrow()
+        val newUri = recipeImageLocalDataSource.convertToInternalStorageUri(byteArray, fileName)
+            .getOrThrow()
+        recipeImageLocalDataSource.copyExif(uri, newUri)
+            .getOrThrow()
+        newUri
     }
 }
