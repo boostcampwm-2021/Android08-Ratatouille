d28a30a5e882eaea448c69ea2a16963d6ef7a450
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-17 23:21:16 +0900

    refactor: calculateTotalTime 함수 변경 #103

--- presentation/src/main/java/com/kdjj/presentation/common/Utils.kt
+++ presentation/src/main/java/com/kdjj/presentation/common/Utils.kt
@@ -2,14 +2,10 @@ package com.kdjj.presentation.common
 
 import com.kdjj.domain.model.Recipe
 
-
-internal fun calculateSeconds(min: Int, sec: Int): Int{
-    return min*60 + sec
+internal fun calculateSeconds(min: Int, sec: Int): Int {
+    return min * 60 + sec
 }
 
-internal fun calculateTotalTime(recipe: Recipe): String {
-    val secs = recipe.stepList.map { it.seconds }.reduce { acc, i -> acc + i }
-    val min = secs / 60
-    return if (min == 0) secs.toString() + "초"
-    else "$min 분 ${secs%60} 초"
+internal fun calculateTotalTime(recipe: Recipe?): Int {
+    return recipe?.let { it.stepList.map { it.seconds }.reduce { acc, i -> acc + i } } ?: 0
 }
