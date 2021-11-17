c255846db9b5ffd3d5a0be50312a1daa08227de0
dongkey <utdhkim94@gmail.com>
2021-11-17 14:00:07 +0900

    refactor: bottom navigation bar 중복 클릭안되도록 수정 #97

--- presentation/src/main/java/com/kdjj/presentation/view/home/HomeActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/HomeActivity.kt
@@ -1,14 +1,11 @@
 package com.kdjj.presentation.view.home
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kdjj.presentation.R
 import com.kdjj.presentation.databinding.ActivityHomeBinding
-import com.kdjj.presentation.view.recipedetail.RecipeDetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,5 +24,6 @@ class HomeActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentContainer_homeActivity) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setOnItemReselectedListener { }
     }
 }
