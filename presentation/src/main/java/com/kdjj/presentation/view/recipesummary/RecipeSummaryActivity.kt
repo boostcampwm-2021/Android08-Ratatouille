d56f06d2579e3089aa336ed4d14268f4bbc7f677
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-17 16:29:00 +0900

    feat: 레시피 개요 화면 초기 생성 #103

---
+++ presentation/src/main/java/com/kdjj/presentation/view/recipesummary/RecipeSummaryActivity.kt
@@ -0,0 +1,24 @@
+package com.kdjj.presentation.view.recipesummary
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.kdjj.presentation.R
+import com.kdjj.presentation.databinding.ActivityRecipeSummaryBinding
+import com.kdjj.presentation.viewmodel.recipesummary.RecipeSummaryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecipeSummaryActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityRecipeSummaryBinding
+    private val recipeSummaryViewModel: RecipeSummaryViewModel by viewModels()
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_summary)
+        binding.viewModel = recipeSummaryViewModel
+        binding.lifecycleOwner = this
+    }
+}
