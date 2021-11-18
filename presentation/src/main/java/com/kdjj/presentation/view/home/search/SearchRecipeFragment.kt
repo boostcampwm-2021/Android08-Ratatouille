921635a1d3be704d867d70029d9b027d8234dfab
K.H.Park <svclaw2000@gmail.com>
2021-11-18 11:15:23 +0900

    feat: show keyboard when enter search #107

--- presentation/src/main/java/com/kdjj/presentation/view/home/search/SearchRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/search/SearchRecipeFragment.kt
@@ -1,10 +1,12 @@
 package com.kdjj.presentation.view.home.search
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.kdjj.presentation.R
 import com.kdjj.presentation.databinding.FragmentSearchRecipeBinding
@@ -25,7 +27,13 @@ class SearchRecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        focusInput()
+    }
+
+    private fun focusInput() {
         binding.editTextSearch.requestFocus()
+        val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager ?: return
+        inputMethodManager.showSoftInput(binding.editTextSearch, InputMethodManager.SHOW_IMPLICIT)
     }
 
     override fun onDestroyView() {
