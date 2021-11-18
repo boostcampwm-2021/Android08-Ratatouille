604254b4cb54ecc32e5ed477d835b403536ead0a
K.H.Park <svclaw2000@gmail.com>
2021-11-18 14:42:33 +0900

    refactor: separate params into multi lines #107

--- presentation/src/main/java/com/kdjj/presentation/view/dialog/ConfirmDialogBuilder.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/dialog/ConfirmDialogBuilder.kt
@@ -8,7 +8,12 @@ import com.kdjj.presentation.model.ConfirmDialogModel
 
 internal object ConfirmDialogBuilder {
 
-    fun create(context: Context, title: String, content: String, onConfirmListener: () -> Unit) {
+    fun create(
+        context: Context,
+        title: String,
+        content: String,
+        onConfirmListener: () -> Unit
+    ) {
         val binding = DialogConfirmBinding.inflate(LayoutInflater.from(context))
         val dialog = AlertDialog.Builder(context)
             .setView(binding.root)
