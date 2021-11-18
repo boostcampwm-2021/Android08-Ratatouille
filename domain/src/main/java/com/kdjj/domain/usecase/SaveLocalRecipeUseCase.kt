2d2ebd7dc35af8772f132aa701b8832a1e778f48
dongkey <utdhkim94@gmail.com>
2021-11-18 19:05:39 +0900

    refactor: FireStore 불러온 데이터 Local에 저장 구현 close #121

--- domain/src/main/java/com/kdjj/domain/usecase/SaveLocalRecipeUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/SaveLocalRecipeUseCase.kt
@@ -9,28 +9,38 @@ internal class SaveLocalRecipeUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository,
     private val imageRepository: RecipeImageRepository
 ) : UseCase<SaveLocalRecipeRequest, Boolean> {
-    
+
     override suspend fun invoke(request: SaveLocalRecipeRequest): Result<Boolean> =
         kotlin.runCatching {
             val recipe = request.recipe
             val recipeImageUri = when (recipe.imgPath.isNotEmpty()) {
                 true -> {
-                    imageRepository.copyExternalImageToInternal(recipe.imgPath, recipe.recipeId)
-                        .getOrThrow()
+                    if (recipe.imgPath.startsWith("https://") || recipe.imgPath.startsWith("gs://")) {
+                        imageRepository.copyRemoteImageToInternal(recipe.imgPath, recipe.recipeId)
+                            .getOrThrow()
+                    } else {
+                        imageRepository.copyExternalImageToInternal(recipe.imgPath, recipe.recipeId)
+                            .getOrThrow()
+                    }
                 }
                 false -> ""
             }
             val recipeStepList = recipe.stepList.map { step ->
                 val stepImageUri = when (step.imgPath.isNotEmpty()) {
                     true -> {
-                        imageRepository.copyExternalImageToInternal(step.imgPath, step.stepId)
-                            .getOrThrow()
+                        if (recipe.imgPath.startsWith("https://") || recipe.imgPath.startsWith("gs://")) {
+                            imageRepository.copyRemoteImageToInternal(step.imgPath, step.stepId)
+                                .getOrThrow()
+                        } else {
+                            imageRepository.copyExternalImageToInternal(step.imgPath, step.stepId)
+                                .getOrThrow()
+                        }
                     }
                     false -> ""
                 }
                 step.copy(imgPath = stepImageUri)
             }
-            
+
             recipeRepository.saveLocalRecipe(
                 recipe.copy(
                     imgPath = recipeImageUri,
