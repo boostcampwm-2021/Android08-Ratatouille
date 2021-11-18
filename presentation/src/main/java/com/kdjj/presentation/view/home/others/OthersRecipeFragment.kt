bc0efe74bec25f558678320ee274d6d1ce41ae7e
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-18 11:16:29 +0900

    refactor: 하드 코딩 수정 #112

--- presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/others/OthersRecipeFragment.kt
@@ -117,10 +117,10 @@ class OthersRecipeFragment : Fragment() {
         viewModel.eventException.observe(viewLifecycleOwner, EventObserver {
             when (it) {
                 is NetworkException -> {
-                    showSnackBar("네트워크와 연결이 끊어졌습니다.")
+                    showSnackBar(getString(R.string.networkErrorMessage))
                 }
                 is ApiException -> {
-                    showSnackBar("서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.")
+                    showSnackBar(getString(R.string.severErrorMessage))
                 }
             }
         })
