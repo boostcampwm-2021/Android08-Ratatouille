4bf54fbcf13119f0370fc1e53277cd66c8bf1c51
dongkey <utdhkim94@gmail.com>
2021-11-18 20:22:57 +0900

    refactor: UploadRecipeUseCase 내부 저장소 이미지 FireStorage에 저장 코드 추가 #123

--- domain/src/main/java/com/kdjj/domain/usecase/UploadRecipeUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/UploadRecipeUseCase.kt
@@ -1,14 +1,47 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.model.RecipeState
 import com.kdjj.domain.repository.RecipeRepository
 import com.kdjj.domain.model.request.UploadRecipeRequest
+import com.kdjj.domain.repository.RecipeImageRepository
 import javax.inject.Inject
 
 class UploadRecipeUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+    private val recipeRepository: RecipeRepository,
+    private val recipeImageRepository: RecipeImageRepository
 ) : UseCase<UploadRecipeRequest, Unit> {
-    
-    override suspend fun invoke(request: UploadRecipeRequest): Result<Unit> {
-        return recipeRepository.uploadRecipe(request.recipe)
-    }
+
+    override suspend fun invoke(request: UploadRecipeRequest): Result<Unit> =
+        runCatching {
+            val recipe = request.recipe
+            val recipeImageUri = when (recipe.imgPath.isNotEmpty()) {
+                true -> {
+                    recipeImageRepository.convertInternalUriToRemoteStorageUri(recipe.imgPath)
+                        .getOrThrow()
+                }
+                false -> ""
+            }
+            val recipeStepList = recipe.stepList.map { step ->
+                val stepImageUri = when (step.imgPath.isNotEmpty()) {
+                    true -> {
+                        recipeImageRepository.convertInternalUriToRemoteStorageUri(step.imgPath)
+                            .getOrThrow()
+                    }
+                    false -> ""
+                }
+                step.copy(imgPath = stepImageUri)
+            }
+            recipeRepository.uploadRecipe(
+                recipe.copy(
+                    imgPath = recipeImageUri,
+                    stepList = recipeStepList,
+                )
+            ).getOrThrow()
+
+            recipeRepository.updateLocalRecipe(
+                recipe.copy(
+                    state = RecipeState.UPLOAD
+                )
+            ).getOrThrow()
+        }
 }
