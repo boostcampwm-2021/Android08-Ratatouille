02f991b7b868f43c7f5b131fa10e203cd97e3cfb
K.H.Park <svclaw2000@gmail.com>
2021-11-19 02:08:29 +0900

    fix: add copy image to update remote #132

--- domain/src/main/java/com/kdjj/domain/usecase/UpdateRemoteRecipeUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/UpdateRemoteRecipeUseCase.kt
@@ -1,15 +1,42 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.model.RecipeState
 import com.kdjj.domain.repository.RecipeRepository
 import com.kdjj.domain.model.request.UpdateRemoteRecipeRequest
+import com.kdjj.domain.repository.RecipeImageRepository
 import javax.inject.Inject
 
 class UpdateRemoteRecipeUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+    private val recipeRepository: RecipeRepository,
+    private val recipeImageRepository: RecipeImageRepository
 ) : UseCase<UpdateRemoteRecipeRequest, Unit> {
     
-    override suspend fun invoke(request: UpdateRemoteRecipeRequest): Result<Unit> {
-        // TODO : workmanager를 이용한 백그라운드 처리
-        return recipeRepository.uploadRecipe(request.recipe)
-    }
+    override suspend fun invoke(request: UpdateRemoteRecipeRequest): Result<Unit> =
+        runCatching {
+            val recipe = request.recipe
+            val recipeImageUri = when (recipe.imgPath.isNotEmpty()) {
+                true -> {
+                    recipeImageRepository.convertInternalUriToRemoteStorageUri(recipe.imgPath)
+                        .getOrThrow()
+                }
+                false -> ""
+            }
+            val recipeStepList = recipe.stepList.map { step ->
+                val stepImageUri = when (step.imgPath.isNotEmpty()) {
+                    true -> {
+                        recipeImageRepository.convertInternalUriToRemoteStorageUri(step.imgPath)
+                            .getOrThrow()
+                    }
+                    false -> ""
+                }
+                step.copy(imgPath = stepImageUri)
+            }
+            recipeRepository.uploadRecipe(
+                recipe.copy(
+                    imgPath = recipeImageUri,
+                    stepList = recipeStepList,
+                    createTime = System.currentTimeMillis()
+                )
+            ).getOrThrow()
+        }
 }
