761d86cc54aa41e85a29cc30c58bdee3de6c11eb
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-18 14:16:04 +0900

    fix: RingtoneManager 생성할때 변수로 받지 않도록 처리 #109

--- presentation/src/main/java/com/kdjj/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/com/kdjj/presentation/di/PresentationModule.kt
@@ -3,7 +3,6 @@ package com.kdjj.presentation.di
 import android.content.Context
 import android.media.Ringtone
 import android.media.RingtoneManager
-import android.net.Uri
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,11 +15,9 @@ class PresentationModule {
 
     @Provides
     fun provideRingTone(@ApplicationContext context: Context): Ringtone {
-        val notification1: Uri =
-        RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         return RingtoneManager.getRingtone(
             context,
-            notification1
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         )
     }
 }
