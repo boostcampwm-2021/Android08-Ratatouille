ae1e5b7379178c5d122bd5240bd2114fc2dc5a97
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-21 19:59:52 +0900

    fix: 최근 레시피 검색 firstore paging 처리 snapshot 으로 수정 #147

--- remote/src/main/java/com/kdjj/remote/datasource/RecipeListRemoteDataSourceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/datasource/RecipeListRemoteDataSourceImpl.kt
@@ -1,19 +1,36 @@
 package com.kdjj.remote.datasource
 
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.kdjj.data.common.errorMap
 import com.kdjj.data.datasource.RecipeListRemoteDataSource
 import com.kdjj.domain.model.Recipe
+import com.kdjj.domain.model.exception.ApiException
+import com.kdjj.domain.model.exception.NetworkException
 import com.kdjj.remote.dao.RemoteRecipeListService
+import java.lang.Exception
 import javax.inject.Inject
 
 internal class RecipeListRemoteDataSourceImpl @Inject constructor(
     private val recipeListService: RemoteRecipeListService,
 ) : RecipeListRemoteDataSource {
-    
+
     override suspend fun fetchLatestRecipeListAfter(
         refresh: Boolean
     ): Result<List<Recipe>> =
         runCatching {
             recipeListService.fetchLatestRecipeListAfter(refresh)
+        }.errorMap {
+            when (it) {
+                is FirebaseFirestoreException -> {
+                    when (it.code.value()) {
+                        14 -> NetworkException()
+                        else -> ApiException()
+                    }
+                }
+                else -> {
+                    Exception(it)
+                }
+            }
         }
     
     override suspend fun fetchPopularRecipeListAfter(
