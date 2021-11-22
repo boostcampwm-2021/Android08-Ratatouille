2407886cc57e20e73221fc9f96e535441211c266
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-22 18:04:47 +0900

    refactor: UseCase 의존성 주입 모듈 분리 #156

--- app/src/main/java/com/kdjj/ratatouille/di/domain/DomainLayerModule.kt
+++ app/src/main/java/com/kdjj/ratatouille/di/domain/DomainLayerModule.kt
@@ -1,10 +1,10 @@
 package com.kdjj.ratatouille.di.domain
 
-import com.kdjj.domain.di.UseCaseModule
+import com.kdjj.domain.di.ResultUseCaseModule
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-@Module(includes = [UseCaseModule::class])
+@Module(includes = [ResultUseCaseModule::class])
 @InstallIn(SingletonComponent::class)
 class DomainLayerModule
