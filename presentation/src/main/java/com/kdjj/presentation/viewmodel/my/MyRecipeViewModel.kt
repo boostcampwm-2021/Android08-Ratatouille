940791f71f2b4e06661863901b7b84e398964efa
dongkey <utdhkim94@gmail.com>
2021-11-22 14:59:45 +0900

    refactor: Log 라인 사제 #154

--- presentation/src/main/java/com/kdjj/presentation/viewmodel/my/MyRecipeViewModel.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/my/MyRecipeViewModel.kt
@@ -1,6 +1,5 @@
 package com.kdjj.presentation.viewmodel.my
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
