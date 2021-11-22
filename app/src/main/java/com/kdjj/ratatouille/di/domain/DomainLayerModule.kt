906956b776cf7be7e5897dd21b2a2da53cc3f236
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-22 18:08:11 +0900

    feat: App 의존성 주입에 FlowUseCaseModule 추가 #156

--- app/src/main/java/com/kdjj/ratatouille/di/domain/DomainLayerModule.kt
+++ app/src/main/java/com/kdjj/ratatouille/di/domain/DomainLayerModule.kt
@@ -1,10 +1,11 @@
 package com.kdjj.ratatouille.di.domain
 
+import com.kdjj.domain.di.FlowUseCaseModule
 import com.kdjj.domain.di.ResultUseCaseModule
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-@Module(includes = [ResultUseCaseModule::class])
+@Module(includes = [ResultUseCaseModule::class, FlowUseCaseModule::class])
 @InstallIn(SingletonComponent::class)
 class DomainLayerModule
