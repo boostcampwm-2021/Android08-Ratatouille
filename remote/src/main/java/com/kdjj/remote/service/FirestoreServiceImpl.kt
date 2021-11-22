69b2e13d6aadf654d1737a7db96f4637c18bdaa8
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-22 22:08:59 +0900

    refactor: Dao에서 domain 자료형으로 반환하던 부분 dto 반환하도록 변경 #156

--- remote/src/main/java/com/kdjj/remote/service/FirestoreServiceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/service/FirestoreServiceImpl.kt
@@ -2,9 +2,7 @@ package com.kdjj.remote.service
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
-import com.kdjj.domain.model.RecipeType
 import com.kdjj.remote.dto.RecipeTypeDto
-import com.kdjj.remote.dto.toDomain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -13,19 +11,19 @@ import javax.inject.Inject
 internal class FirestoreServiceImpl @Inject constructor(
     private val firestore: FirebaseFirestore
 ) : FirestoreService {
-    
-    override suspend fun fetchRecipeTypes(): List<RecipeType> =
+
+    override suspend fun fetchRecipeTypes(): List<RecipeTypeDto> =
         withContext(Dispatchers.IO) {
             firestore.collection(RECIPE_TYPE_COLLECTION_ID)
                 .get()
                 .await()
                 .map { queryDocumentSnapshot ->
-                    queryDocumentSnapshot.toObject<RecipeTypeDto>().toDomain()
+                    queryDocumentSnapshot.toObject<RecipeTypeDto>()
                 }
         }
-    
+
     companion object {
-        
+
         const val RECIPE_TYPE_COLLECTION_ID = "recipeType"
     }
 }
--- remote/src/main/java/com/kdjj/remote/service/RecipeServiceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/service/RecipeServiceImpl.kt
@@ -3,10 +3,7 @@ package com.kdjj.remote.service
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.toObject
-import com.kdjj.domain.model.Recipe
 import com.kdjj.remote.dto.RecipeDto
-import com.kdjj.remote.dto.toDomain
-import com.kdjj.remote.dto.toDto
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -16,18 +13,18 @@ internal class RecipeServiceImpl @Inject constructor(
     private val firestore: FirebaseFirestore
 ) : RemoteRecipeService {
 
-    override suspend fun uploadRecipe(recipe: Recipe): Unit =
+    override suspend fun uploadRecipe(recipeDto: RecipeDto): Unit =
         withContext(Dispatchers.IO) {
             firestore.collection(RECIPE_COLLECTION_ID)
-                .document(recipe.recipeId)
-                .set(recipe.toDto())
+                .document(recipeDto.recipeId)
+                .set(recipeDto)
                 .await()
         }
 
-    override suspend fun increaseViewCount(recipe: Recipe): Unit =
+    override suspend fun increaseViewCount(recipeDto: RecipeDto): Unit =
         withContext(Dispatchers.IO) {
             val documentRefer = firestore.collection(RECIPE_COLLECTION_ID)
-                .document(recipe.recipeId)
+                .document(recipeDto.recipeId)
             firestore.runTransaction { transaction ->
                 val snapshot = transaction.get(documentRefer)
                 val newRecipeViewCount = snapshot.getLong("viewCount")!! + 1
@@ -35,21 +32,21 @@ internal class RecipeServiceImpl @Inject constructor(
             }.await()
         }
 
-    override suspend fun deleteRecipe(recipe: Recipe): Unit =
+    override suspend fun deleteRecipe(recipeDto: RecipeDto): Unit =
         withContext(Dispatchers.IO) {
             firestore.collection(RECIPE_COLLECTION_ID)
-                .document(recipe.recipeId)
+                .document(recipeDto.recipeId)
                 .delete()
                 .await()
         }
 
-    override suspend fun fetchRecipe(recipeID: String): Recipe =
+    override suspend fun fetchRecipe(recipeID: String): RecipeDto =
         withContext(Dispatchers.IO) {
             val documentSnapShot = firestore.collection(RECIPE_COLLECTION_ID)
                 .document(recipeID)
                 .get(Source.SERVER)
                 .await()
-            documentSnapShot.toObject<RecipeDto>()?.toDomain() ?: throw Exception()
+            documentSnapShot.toObject<RecipeDto>() ?: throw Exception()
         }
 
     companion object {
