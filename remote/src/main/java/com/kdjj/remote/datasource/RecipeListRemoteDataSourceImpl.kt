1c883dfceb651225d16ec16121bb6c8571380dbc
dongkey <utdhkim94@gmail.com>
2021-11-22 15:58:33 +0900

    refactor: suspendCancellableCoroutine 제거 #157

--- remote/src/main/java/com/kdjj/remote/datasource/RecipeListRemoteDataSourceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/datasource/RecipeListRemoteDataSourceImpl.kt
@@ -1,13 +1,10 @@
 package com.kdjj.remote.datasource
 
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.kdjj.data.common.errorMap
 import com.kdjj.data.datasource.RecipeListRemoteDataSource
 import com.kdjj.domain.model.Recipe
-import com.kdjj.domain.model.exception.ApiException
-import com.kdjj.domain.model.exception.NetworkException
+import com.kdjj.remote.common.fireStoreExceptionToDomain
 import com.kdjj.remote.dao.RemoteRecipeListService
-import java.lang.Exception
 import javax.inject.Inject
 
 internal class RecipeListRemoteDataSourceImpl @Inject constructor(
@@ -40,18 +37,5 @@ internal class RecipeListRemoteDataSourceImpl @Inject constructor(
             recipeListService.fetchSearchRecipeListAfter(keyword, refresh)
         }.errorMap {
             fireStoreExceptionToDomain(it)
-        }
-
-    private fun fireStoreExceptionToDomain(throwable: Throwable) =
-        when (throwable) {
-            is FirebaseFirestoreException -> {
-                when (throwable.code.value()) {
-                    14 -> NetworkException()
-                    else -> ApiException()
-                }
-            }
-            else -> {
-                Exception(throwable)
-            }
         }
 }
