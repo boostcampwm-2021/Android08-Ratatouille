b68ebd0910892ce122eb5912a23f98acf32ee98a
dongkey <utdhkim94@gmail.com>
2021-11-22 17:51:37 +0900

    refactor: SpanCount 및 마진값 고정 close #160

--- presentation/src/main/java/com/kdjj/presentation/view/home/my/SpacesItemDecoration.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/home/my/SpacesItemDecoration.kt
@@ -5,9 +5,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class SpacesItemDecoration(
-    private val spanCount: Int,
-    private val totalWidth: Int,
-    private val itemWidth: Int
+    private val space: Int,
 ) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(
@@ -16,22 +14,12 @@ class SpacesItemDecoration(
         parent: RecyclerView,
         state: RecyclerView.State
     ) {
-        val position = parent.getChildAdapterPosition(view)
-        val space = (totalWidth - itemWidth * spanCount) / (spanCount + 1)
 
-        when (position % spanCount) {
-            0 -> {
-                outRect.left = space
-                outRect.right = space / 2
-            }
-            spanCount - 1 -> {
-                outRect.left = space / 2
-                outRect.right = space
-            }
-            else -> {
-                outRect.left = space / 2
-                outRect.right = space / 2
-            }
+        outRect.apply {
+            top = space
+            bottom = space
+            left = space
+            right = space
         }
     }
 }
