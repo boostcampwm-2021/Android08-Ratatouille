47e68b33b86b83df3df81ff029771f8ee02dc6a2
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-23 01:28:17 +0900

    test(RecipeTypeRemoetDataSourceImplTest): 반환값 수정에 따른 테스트 코드 수정 close #156

--- remote/src/test/java/com/kdjj/remote/recipe/RecipeTypeRemoteDataSourceImplTest.kt
+++ remote/src/test/java/com/kdjj/remote/recipe/RecipeTypeRemoteDataSourceImplTest.kt
@@ -1,8 +1,9 @@
 package com.kdjj.remote.recipe
 
-import com.kdjj.domain.model.RecipeType
-import com.kdjj.remote.service.FirestoreServiceImpl
 import com.kdjj.remote.datasource.RecipeTypeRemoteDataSourceImpl
+import com.kdjj.remote.dto.RecipeTypeDto
+import com.kdjj.remote.dto.toDomain
+import com.kdjj.remote.service.FirestoreServiceImpl
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -11,31 +12,32 @@ import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
 class RecipeTypeRemoteDataSourceImplTest {
-	
-	private lateinit var mockFireStoreDaoImpl: FirestoreServiceImpl
-	private lateinit var recipeRemoteDataSourceImpl: RecipeTypeRemoteDataSourceImpl
-	private val testRecipeTypeList = listOf(
-		RecipeType(1, "한식"),
-		RecipeType(1, "중식"),
-		RecipeType(1, "양식")
-	)
-	
-	@Before
-	fun setup() {
-		// given
-		mockFireStoreDaoImpl = mock(FirestoreServiceImpl::class.java)
-		recipeRemoteDataSourceImpl = RecipeTypeRemoteDataSourceImpl(mockFireStoreDaoImpl)
-	}
-	
-	@Test
-	fun fetchRecipeTypes_getRecipeTypeList_true(): Unit = runBlocking {
-		// given
-		`when`(mockFireStoreDaoImpl.fetchRecipeTypes()).thenReturn(testRecipeTypeList)
-		
-		// when
-		val recipeTypeList = recipeRemoteDataSourceImpl.fetchRecipeTypeList()
-		println(recipeTypeList)
-		// then
-		assertEquals(Result.success(testRecipeTypeList), recipeTypeList)
-	}
+
+    private lateinit var mockFireStoreDaoImpl: FirestoreServiceImpl
+    private lateinit var recipeRemoteDataSourceImpl: RecipeTypeRemoteDataSourceImpl
+    private val testRecipeTypeDtoList = listOf(
+        RecipeTypeDto(1, "한식"),
+        RecipeTypeDto(1, "중식"),
+        RecipeTypeDto(1, "양식")
+    )
+    private val testRecipeTypeList = testRecipeTypeDtoList.map { it.toDomain() }
+
+    @Before
+    fun setup() {
+        // given
+        mockFireStoreDaoImpl = mock(FirestoreServiceImpl::class.java)
+        recipeRemoteDataSourceImpl = RecipeTypeRemoteDataSourceImpl(mockFireStoreDaoImpl)
+    }
+
+    @Test
+    fun fetchRecipeTypes_getRecipeTypeList_true(): Unit = runBlocking {
+        // given
+        `when`(mockFireStoreDaoImpl.fetchRecipeTypes()).thenReturn(testRecipeTypeDtoList)
+
+        // when
+        val recipeTypeList = recipeRemoteDataSourceImpl.fetchRecipeTypeList()
+
+        // then
+        assertEquals(Result.success(testRecipeTypeList), recipeTypeList)
+    }
 }
