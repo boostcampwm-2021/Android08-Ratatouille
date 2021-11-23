6358c2b8b47dcee64a01a427214841c9566d729a
K.H.Park <svclaw2000@gmail.com>
2021-11-24 01:33:59 +0900

    feat: add recipe temp dao in local #176

---
+++ local/src/main/java/com/kdjj/local/dao/RecipeTempDao.kt
@@ -0,0 +1,33 @@
+package com.kdjj.local.dao
+
+import androidx.room.*
+import com.kdjj.domain.model.Recipe
+import com.kdjj.local.dto.*
+
+@Dao
+internal interface RecipeTempDao {
+    
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipeTempMeta(recipeTempMeta: RecipeTempMetaDto)
+    
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipeTempStep(recipeTempStep: RecipeTempStepDto)
+
+    @Transaction
+    suspend fun insertRecipeTemp(recipe: Recipe) {
+        deleteTempStepList(recipe.recipeId)
+        insertRecipeTempMeta(recipe.toTempDto())
+        recipe.stepList.forEachIndexed { index, recipeStep ->
+            insertRecipeTempStep(recipeStep.toTempDto(recipe.recipeId, index + 1))
+        }
+    }
+    
+    @Query("DELETE FROM RecipeTempStep WHERE parentRecipeId = :recipeId")
+    suspend fun deleteTempStepList(recipeId: String)
+    
+    @Delete
+    suspend fun deleteRecipeTemp(recipeTempMeta: RecipeTempMetaDto)
+    
+    @Query("SELECT * FROM RecipeTempMeta WHERE recipeMetaId = :recipeId")
+    fun getRecipeTemp(recipeId: String): RecipeTempDto?
+}
--- local/src/main/java/com/kdjj/local/database/RecipeDatabase.kt
+++ local/src/main/java/com/kdjj/local/database/RecipeDatabase.kt
@@ -2,29 +2,31 @@ package com.kdjj.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.kdjj.local.dao.ImageValidationDao
-import com.kdjj.local.dao.RecipeDao
-import com.kdjj.local.dao.RecipeListDao
-import com.kdjj.local.dao.RecipeTypeDao
-import com.kdjj.local.dto.ImageValidationDto
-import com.kdjj.local.dto.RecipeMetaDto
-import com.kdjj.local.dto.RecipeStepDto
-import com.kdjj.local.dto.RecipeTypeDto
+import com.kdjj.local.dao.*
+import com.kdjj.local.dto.*
 
 @Database(
-    entities = [RecipeMetaDto::class, RecipeTypeDto::class, RecipeStepDto::class, ImageValidationDto::class],
+    entities = [
+        RecipeMetaDto::class,
+        RecipeTypeDto::class,
+        RecipeStepDto::class,
+        ImageValidationDto::class,
+        RecipeTempMetaDto::class,
+        RecipeTempStepDto::class
+    ],
     version = 1,
     exportSchema = false
 )
 internal abstract class RecipeDatabase : RoomDatabase() {
-    
+
     internal abstract fun getRecipeDao(): RecipeDao
+    internal abstract fun getRecipeTempDao(): RecipeTempDao
     internal abstract fun getRecipeListDao(): RecipeListDao
     internal abstract fun getRecipeTypeDao(): RecipeTypeDao
     internal abstract fun getImageValidationDao(): ImageValidationDao
-    
+
     companion object {
-        
+
         const val RECIPE_DATABASE_NAME = "RecipeDatabase.db"
     }
 }
