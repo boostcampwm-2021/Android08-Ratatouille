a8ec66ec553af50d57720de3b4fe615576ecf71c
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-24 17:49:52 +0900

    feat: 힐끔보기 화면 throttle 처리 #186

--- remote/src/main/java/com/kdjj/remote/common/Utils.kt
+++ remote/src/main/java/com/kdjj/remote/common/Utils.kt
@@ -4,6 +4,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException
 import com.kdjj.domain.model.exception.ApiException
 import com.kdjj.domain.model.exception.NetworkException
 import java.lang.Exception
+import java.util.concurrent.CancellationException
 
 internal fun fireStoreExceptionToDomain(throwable: Throwable) =
     when (throwable) {
@@ -13,6 +14,7 @@ internal fun fireStoreExceptionToDomain(throwable: Throwable) =
                 else -> ApiException()
             }
         }
+        is CancellationException -> throwable
         else -> {
             Exception(throwable)
         }
