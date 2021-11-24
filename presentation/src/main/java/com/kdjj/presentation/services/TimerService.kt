b21ae369f33cd7793bc90dae017205f80a29c38b
dongkey <utdhkim94@gmail.com>
2021-11-25 02:12:36 +0900

    refactor: 리뷰 반영 #177

--- presentation/src/main/java/com/kdjj/presentation/services/TimerService.kt
+++ presentation/src/main/java/com/kdjj/presentation/services/TimerService.kt
@@ -28,43 +28,37 @@ class TimerService : LifecycleService() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let { intent ->
-            when (intent.action) {
-                ACTION_START -> {
-                    val timerStrList = intent.getStringArrayExtra("TIMERS") ?: arrayOf()
+            if (intent.action == "ACTION_START") {
+                val timerStrList = intent.getStringArrayExtra("TIMERS") ?: arrayOf()
 
-                    val timerList = timerStrList.map { timerStr ->
-                        val (timeStr, stepId, stepName) = timerStr.split(":", limit = 3)
-                        StepTimerItem(timeStr.toInt(), stepId, stepName)
-                    }.sortedByDescending { it.time }
+                val timerList = timerStrList.map { timerStr ->
+                    val (timeStr, stepId, stepName) = timerStr.split(":", limit = 3)
+                    StepTimerItem(timeStr.toInt(), stepId, stepName)
+                }.sortedByDescending { it.time }
 
-                    val checkAlarms = hashSetOf<String>()
-                    val maxMillis = (timerList.maxOf { it.time } + 1) * 1000L
-                    serviceTimer = ServiceTimer(maxMillis) { millisLeft ->
-                        val timeElapsed = maxMillis - millisLeft
-                        timerList.forEach { item ->
-                            val timeLeft = (item.time - timeElapsed / 1000).toInt()
-                            if (timeLeft >= 0) {
-                                Notifications.showTimer(
-                                    applicationContext,
-                                    item.stepId,
-                                    item.stepName,
-                                    timeLeft
-                                )
-                            } else {
-                                if(!checkAlarms.contains(item.stepId)){
-                                    Notifications.showAlarm(
-                                        applicationContext,
-                                        item.stepId,
-                                        item.stepName
-                                    )
-                                }
-                                checkAlarms.add(item.stepId)
-                            }
+                val checkAlarms = hashSetOf<String>()
+                val maxMillis = (timerList.maxOf { it.time } + 1) * 1000L
+                serviceTimer = ServiceTimer(maxMillis) { millisLeft ->
+                    val timeElapsed = maxMillis - millisLeft
+                    timerList.forEach { item ->
+                        val timeLeft = (item.time - timeElapsed / 1000).toInt()
+                        if (timeLeft >= 0) {
+                            Notifications.showTimer(
+                                applicationContext,
+                                item.stepId,
+                                item.stepName,
+                                timeLeft
+                            )
+                        } else if (!checkAlarms.contains(item.stepId)) {
+                            Notifications.showAlarm(
+                                applicationContext,
+                                item.stepId,
+                                item.stepName
+                            )
                         }
-                    }.apply { start() }
-                }
-                else -> {
-                }
+                        checkAlarms.add(item.stepId)
+                    }
+                }.apply { start() }
             }
         }
         return super.onStartCommand(intent, flags, startId)
