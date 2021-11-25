4ddcd6038fdf972574dc737a703838aa1653e72c
K.H.Park <svclaw2000@gmail.com>
2021-11-25 17:39:41 +0900

    fix: fix space under last step #194

--- presentation/src/main/java/com/kdjj/presentation/view/recipedetail/StepItemDecoration.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipedetail/StepItemDecoration.kt
@@ -2,9 +2,11 @@ package com.kdjj.presentation.view.recipedetail
 
 import android.graphics.Rect
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class StepItemDecoration : RecyclerView.ItemDecoration() {
+
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -12,7 +14,26 @@ class StepItemDecoration : RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
         if (parent.getChildAdapterPosition(view) == (parent.adapter?.itemCount ?: 0) - 1) {
+            if (view.height == 0) fixLayoutSize(view, parent)
             outRect.bottom = parent.height - view.height
         }
+    }
+
+    private fun fixLayoutSize(view: View, parent: ViewGroup) {
+        if (view.layoutParams == null) {
+            view.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY)
+        val heightSpec = View.MeasureSpec.makeMeasureSpec(parent.height, View.MeasureSpec.UNSPECIFIED)
+        val childWidth = ViewGroup.getChildMeasureSpec(
+            widthSpec, parent.paddingLeft + parent.paddingRight, view.layoutParams.width
+        )
+        val childHeight = ViewGroup.getChildMeasureSpec(
+            heightSpec, parent.paddingTop + parent.paddingBottom, view.layoutParams.height
+        )
+        view.measure(childWidth, childHeight)
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
     }
 }
