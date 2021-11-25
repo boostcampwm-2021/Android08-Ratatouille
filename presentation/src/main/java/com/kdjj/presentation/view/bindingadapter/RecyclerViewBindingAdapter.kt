3e6b2d2e1a415ada564d47ec03aa097f6f79630c
K.H.Park <svclaw2000@gmail.com>
2021-11-26 04:36:50 +0900

    fix: fix space after every timer #222

--- presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/RecyclerViewBindingAdapter.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 @Suppress("UNCHECKED_CAST")
 @BindingAdapter("app:submitList")
 fun <T, VH : RecyclerView.ViewHolder> RecyclerView.submitList(list: List<T>?) {
-    (adapter as? ListAdapter<T, VH>)?.submitList(list)
+    (adapter as? ListAdapter<T, VH>)?.submitList(list) {
+        invalidateItemDecorations()
+    }
 }
 
 @Suppress("UNCHECKED_CAST")
