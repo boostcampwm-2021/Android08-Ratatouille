ed6ce581270912def34337bc109769cceff2dd36
dongkey <utdhkim94@gmail.com>
2021-11-25 16:05:01 +0900

    refactor: 리뷰 반영 #199

--- presentation/src/main/java/com/kdjj/presentation/common/Utils.kt
+++ presentation/src/main/java/com/kdjj/presentation/common/Utils.kt
@@ -7,9 +7,6 @@ const val RECIPE_ID = "recipeID"
 const val RECIPE_STATE = "recipeState"
 const val UPDATED_RECIPE_ID = "updatedRecipeId"
 const val ACTION_START = "ACTION_START"
-const val ANIMATION_DURATION = 800L
-const val SPLASH_TIME = 1500L
-const val ALPHA = "alpha"
 
 internal fun calculateSeconds(min: Int, sec: Int): Int {
     return min * 60 + sec
