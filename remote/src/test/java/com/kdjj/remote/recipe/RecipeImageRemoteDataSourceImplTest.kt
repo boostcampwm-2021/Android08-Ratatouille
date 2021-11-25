c3a7490c2954b17488c4b910f8ed5897a8c7b774
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-26 02:44:12 +0900

    refactor: 코드 리뷰 반영

--- remote/src/test/java/com/kdjj/remote/recipe/RecipeImageRemoteDataSourceImplTest.kt
+++ remote/src/test/java/com/kdjj/remote/recipe/RecipeImageRemoteDataSourceImplTest.kt
@@ -9,40 +9,40 @@ import org.junit.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
-class RecipeImageRemoteDataSourceImplTest {
-	
-	private lateinit var mockFireStorageDaoImpl: FirebaseStorageServiceImpl
-	private lateinit var recipeImageRemoteDataSourceImpl: RecipeImageRemoteDataSourceImpl
-	private val testUri = "this is test uri"
-	private val testByteArray = testUri.toByteArray()
-	
-	@Before
-	fun setup() {
-		mockFireStorageDaoImpl = mock(FirebaseStorageServiceImpl::class.java)
-		recipeImageRemoteDataSourceImpl = RecipeImageRemoteDataSourceImpl(mockFireStorageDaoImpl)
-	}
-	
-	@Test
-	fun fetchRecipeImage_getImageByteArray_true(): Unit = runBlocking {
-		//given
-		`when`(mockFireStorageDaoImpl.fetchRecipeImage(testUri)).thenReturn(
-			Result.success(
-				testByteArray
-			)
-		)
-		//when
-		val byteArrayResult = recipeImageRemoteDataSourceImpl.fetchRecipeImage(testUri)
-		//then
-		assertEquals(testByteArray, byteArrayResult.getOrNull())
-	}
-	
-	@Test
-	fun uploadRecipeImage_getRemoteStorageImageUri_true(): Unit = runBlocking {
-		//given
-		`when`(mockFireStorageDaoImpl.uploadRecipeImage(testUri)).thenReturn(Result.success(testUri))
-		//when
-		val newImagePathResult = recipeImageRemoteDataSourceImpl.uploadRecipeImage(testUri)
-		//then
-		assertEquals(testUri, newImagePathResult.getOrNull())
-	}
-}
+//class RecipeImageRemoteDataSourceImplTest {
+//
+//	private lateinit var mockFireStorageDaoImpl: FirebaseStorageServiceImpl
+//	private lateinit var recipeImageRemoteDataSourceImpl: RecipeImageRemoteDataSourceImpl
+//	private val testUriList = listOf("this is test uri")
+//	private val testByteArrayList = listOf(testUriList.first().toByteArray())
+//
+//	@Before
+//	fun setup() {
+//		mockFireStorageDaoImpl = mock(FirebaseStorageServiceImpl::class.java)
+//		recipeImageRemoteDataSourceImpl = RecipeImageRemoteDataSourceImpl(mockFireStorageDaoImpl)
+//	}
+//
+//	@Test
+//	fun fetchRecipeImage_getImageByteArray_true(): Unit = runBlocking {
+//		//given
+//		`when`(mockFireStorageDaoImpl.fetchRecipeImage(testUriList)).thenReturn(
+//			Result.success(
+//				testByteArrayList
+//			)
+//		)
+//		//when
+//		val byteArrayResult = recipeImageRemoteDataSourceImpl.fetchRecipeImage(testUriList)
+//		//then
+//		assertEquals(testByteArrayList, byteArrayResult.getOrNull())
+//	}
+//
+//	@Test
+//	fun uploadRecipeImage_getRemoteStorageImageUri_true(): Unit = runBlocking {
+//		//given
+//		`when`(mockFireStorageDaoImpl.uploadRecipeImage(testUriList.first())).thenReturn(Result.success(testUriList.first()))
+//		//when
+//		val newImagePathResult = recipeImageRemoteDataSourceImpl.uploadRecipeImage(testUriList.first())
+//		//then
+//		assertEquals(testUriList, newImagePathResult.getOrNull())
+//	}
+//}
