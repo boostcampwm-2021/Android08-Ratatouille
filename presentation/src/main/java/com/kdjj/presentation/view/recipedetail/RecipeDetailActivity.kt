2929feaaa0f9816865b6bf500436578fff05755c
K.H.Park <svclaw2000@gmail.com>
2021-11-25 17:51:37 +0900

    refactor: change method names #194

--- presentation/src/main/java/com/kdjj/presentation/view/recipedetail/RecipeDetailActivity.kt
+++ presentation/src/main/java/com/kdjj/presentation/view/recipedetail/RecipeDetailActivity.kt
@@ -5,7 +5,6 @@ import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
@@ -87,7 +86,7 @@ class RecipeDetailActivity : AppCompatActivity() {
                         ?.findFirstCompletelyVisibleItemPosition()
                         ?: return
 
-                    viewModel.showScrolledTo(idx)
+                    viewModel.updateCurrentStepTo(idx)
                 }
             })
         }
