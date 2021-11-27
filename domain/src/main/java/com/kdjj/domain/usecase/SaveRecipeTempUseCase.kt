3e96d7fcc6374f1d6b1c5a2c6250afe7c6e875cc
K.H.Park <svclaw2000@gmail.com>
2021-11-27 18:48:12 +0900

    fix: add stepId if empty in temp #224

--- domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeTempUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeTempUseCase.kt
@@ -1,13 +1,22 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.common.IdGenerator
 import com.kdjj.domain.model.request.SaveRecipeTempRequest
 import com.kdjj.domain.repository.RecipeTempRepository
 import javax.inject.Inject
 
-class SaveRecipeTempUseCase @Inject constructor(
+internal class SaveRecipeTempUseCase @Inject constructor(
     private val tempRepository: RecipeTempRepository,
+    private val idGenerator: IdGenerator
 ) : ResultUseCase<SaveRecipeTempRequest, Unit> {
 
     override suspend fun invoke(request: SaveRecipeTempRequest): Result<Unit> =
-        tempRepository.saveRecipeTemp(request.recipe)
+        runCatching {
+            val recipe = request.recipe
+            val stepList = recipe.stepList.map { step ->
+                if (step.stepId.isEmpty()) step.copy(stepId = idGenerator.generateId())
+                else step
+            }
+            return tempRepository.saveRecipeTemp(recipe.copy(stepList = stepList))
+        }
 }
