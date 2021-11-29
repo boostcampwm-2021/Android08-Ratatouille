dbec313f95c34aadd789cf04fc082974b103d0ed
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-29 11:36:58 +0900

    refactor(RecipeUploadWorker): 레시피 존재하지 않을 시 재시도 하지 않도록 수정 #191

--- presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeUploadWorker.kt
+++ presentation/src/main/java/com/kdjj/presentation/viewmodel/recipeeditor/RecipeUploadWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.kdjj.domain.model.exception.NotExistRecipeException
 import com.kdjj.domain.model.request.UpdateUploadedRecipeRequest
 import com.kdjj.domain.usecase.ResultUseCase
 import com.kdjj.presentation.common.UPDATED_RECIPE_ID
@@ -19,6 +20,13 @@ class RecipeUploadWorker @AssistedInject constructor(
     override suspend fun doWork(): Result {
         val updatedRecipeId = inputData.getString(UPDATED_RECIPE_ID) ?: return Result.failure()
         val result = updateUploadedRecipeUseCase(UpdateUploadedRecipeRequest(updatedRecipeId))
+            .onFailure {
+                when (it) {
+                    is NotExistRecipeException -> {
+                        return Result.failure()
+                    }
+                }
+            }
         return if (result.isSuccess) Result.success() else Result.retry()
     }
 }
