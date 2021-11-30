96c7deee118ffda166e6afd54ffb71dc65bf6957
K.H.Park <svclaw2000@gmail.com>
2021-11-30 11:33:23 +0900

    refactor: change to flatMap in UploadRecipeUseCase #227

--- domain/src/main/java/com/kdjj/domain/usecase/UploadRecipeUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/UploadRecipeUseCase.kt
@@ -1,5 +1,6 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.common.flatMap
 import com.kdjj.domain.model.RecipeState
 import com.kdjj.domain.model.request.UploadRecipeRequest
 import com.kdjj.domain.repository.RecipeImageRepository
@@ -13,44 +14,44 @@ class UploadRecipeUseCase @Inject constructor(
     private val recipeImageRepository: RecipeImageRepository
 ) : ResultUseCase<UploadRecipeRequest, Unit> {
 
-    override suspend fun invoke(request: UploadRecipeRequest): Result<Unit> =
-        runCatching {
-            val recipe = request.recipe
-
-            val imgList = listOf(recipe.imgPath)
-                .plus(recipe.stepList.map { it.imgPath })
-                .map {
-                    coroutineScope {
-                        async {
-                            convertImageToRemote(it)
-                        }
-                    }
+    override suspend fun invoke(request: UploadRecipeRequest): Result<Unit> {
+        val recipe = request.recipe
+        return (listOf(recipe.imgPath) + recipe.stepList.map { it.imgPath }).map {
+            coroutineScope {
+                async {
+                    convertImageToRemote(it)
                 }
-
-            val recipeStepList = recipe.stepList.mapIndexed { i, step ->
+            }
+        }.fold(Result.success(listOf<String>())) { acc, deferred ->
+            acc.flatMap { imgList ->
+                deferred.await().flatMap { imgPath ->
+                    Result.success(imgList + imgPath)
+                }
+            }
+        }.flatMap { imgList ->
+            val stepList = recipe.stepList.mapIndexed { idx, step ->
                 step.copy(
-                    imgPath = imgList[i + 1].await()
+                    imgPath = imgList[idx + 1]
                 )
             }
-
             recipeRepository.uploadRecipe(
                 recipe.copy(
-                    imgPath = imgList.first().await(),
-                    stepList = recipeStepList,
+                    imgPath = imgList.first(),
+                    stepList = stepList,
                     createTime = System.currentTimeMillis()
                 )
-            ).getOrThrow()
-
-            recipeRepository.updateMyRecipe(
-                recipe.copy(
-                    state = RecipeState.UPLOAD
+            ).onSuccess {
+                recipeRepository.updateMyRecipe(
+                    recipe.copy(
+                        state = RecipeState.UPLOAD
+                    )
                 )
-            ).getOrNull()
+            }
         }
+    }
 
-    private suspend fun convertImageToRemote(imgPath: String): String {
-        return if (imgPath.isEmpty()) ""
+    private suspend fun convertImageToRemote(imgPath: String): Result<String> {
+        return if (imgPath.isEmpty()) Result.success("")
         else recipeImageRepository.convertInternalUriToRemoteStorageUri(imgPath)
-            .getOrThrow()
     }
 }
