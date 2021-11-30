342503f5bbc6a1599cd8c3b4206a5522fd6454cc
K.H.Park <svclaw2000@gmail.com>
2021-11-30 13:53:05 +0900

    refactor: change to flatMap in SaveRecipeTempUseCase #227

--- domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeTempUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/SaveRecipeTempUseCase.kt
@@ -1,6 +1,7 @@
 package com.kdjj.domain.usecase
 
 import com.kdjj.domain.common.IdGenerator
+import com.kdjj.domain.common.flatMap
 import com.kdjj.domain.model.ImageInfo
 import com.kdjj.domain.model.request.SaveRecipeTempRequest
 import com.kdjj.domain.repository.RecipeImageRepository
@@ -13,15 +14,16 @@ internal class SaveRecipeTempUseCase @Inject constructor(
     private val idGenerator: IdGenerator
 ) : ResultUseCase<SaveRecipeTempRequest, Unit> {
 
-    override suspend fun invoke(request: SaveRecipeTempRequest): Result<Unit> =
-        runCatching {
-            val recipe = request.recipe
-            val imgPathList = listOf(recipe.imgPath) + recipe.stepList.map { it.imgPath }
-            val imgInfoList = imgPathList.filter { it.isNotEmpty() }
-                .map { ImageInfo(it, idGenerator.generateId()) }
+    override suspend fun invoke(request: SaveRecipeTempRequest): Result<Unit> {
+        val recipe = request.recipe
+        val imgPathList = listOf(recipe.imgPath) + recipe.stepList.map { it.imgPath }
+        val imgInfoList = imgPathList.filter {
+            it.isNotEmpty()
+        }.map {
+            ImageInfo(it, idGenerator.generateId())
+        }
 
-            val copiedImgPathList = copyImageToInternal(imgInfoList)
-
+        return copyImageToInternal(imgInfoList).flatMap { copiedImgPathList ->
             var i = 0
             val totalImgList = imgPathList.map {
                 if (it.isEmpty()) it else copiedImgPathList[i++]
@@ -37,17 +39,17 @@ internal class SaveRecipeTempUseCase @Inject constructor(
                 )
             }
 
-            return tempRepository.saveRecipeTemp(
+            tempRepository.saveRecipeTemp(
                 recipe.copy(
                     imgPath = totalImgList.first(),
                     stepList = stepList
                 )
             )
         }
+    }
 
-    private suspend fun copyImageToInternal(imgInfoList: List<ImageInfo>): List<String> {
-        if (imgInfoList.isEmpty()) return listOf()
-        return imageRepository.copyExternalImageToInternal(imgInfoList)
-            .getOrThrow()
+    private suspend fun copyImageToInternal(imgInfoList: List<ImageInfo>): Result<List<String>> {
+        return if (imgInfoList.isEmpty()) Result.success(listOf())
+        else imageRepository.copyExternalImageToInternal(imgInfoList)
     }
 }
