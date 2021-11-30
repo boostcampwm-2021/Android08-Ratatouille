a5abc15d11efa74f29f7027979a8ee79d7a20c28
K.H.Park <svclaw2000@gmail.com>
2021-11-30 11:33:31 +0900

    refactor: change to flatMap in FetchRecipeTypeListUseCase #227

--- domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypeListUseCase.kt
+++ domain/src/main/java/com/kdjj/domain/usecase/FetchRecipeTypeListUseCase.kt
@@ -1,5 +1,6 @@
 package com.kdjj.domain.usecase
 
+import com.kdjj.domain.common.handleWith
 import com.kdjj.domain.model.RecipeType
 import com.kdjj.domain.repository.RecipeTypeRepository
 import com.kdjj.domain.model.request.EmptyRequest
@@ -10,16 +11,10 @@ class FetchRecipeTypeListUseCase @Inject constructor(
 ) : ResultUseCase<EmptyRequest, @JvmSuppressWildcards List<RecipeType>> {
 	
 	override suspend fun invoke(request: EmptyRequest): Result<List<RecipeType>> {
-		val remoteResult = recipeTypeRepository.fetchRemoteRecipeTypeList()
-		return when {
-			remoteResult.isSuccess -> {
-				remoteResult.also { result ->
-					recipeTypeRepository.saveRecipeTypeList(result.getOrThrow())
-				}
-			}
-			else -> {
-				recipeTypeRepository.fetchLocalRecipeTypeList()
-			}
+		return recipeTypeRepository.fetchRemoteRecipeTypeList().onSuccess { result ->
+			recipeTypeRepository.saveRecipeTypeList(result)
+		}.handleWith {
+			recipeTypeRepository.fetchLocalRecipeTypeList()
 		}
 	}
 }
