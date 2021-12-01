ef9b69be530af74615ac90ad7733922a1e141423
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-12-02 05:27:28 +0900

    refactor: 오류 코드 명시적으로 변경 #253

--- remote/src/main/java/com/kdjj/remote/common/Utils.kt
+++ remote/src/main/java/com/kdjj/remote/common/Utils.kt
@@ -1,16 +1,16 @@
 package com.kdjj.remote.common
 
 import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.FirebaseFirestoreException.Code.UNAVAILABLE
 import com.kdjj.domain.model.exception.ApiException
 import com.kdjj.domain.model.exception.NetworkException
-import java.lang.Exception
 import java.util.concurrent.CancellationException
 
 internal fun fireStoreExceptionToDomain(throwable: Throwable) =
     when (throwable) {
         is FirebaseFirestoreException -> {
-            when (throwable.code.value()) {
-                14 -> NetworkException()
+            when (throwable.code) {
+                UNAVAILABLE -> NetworkException()
                 else -> ApiException()
             }
         }
