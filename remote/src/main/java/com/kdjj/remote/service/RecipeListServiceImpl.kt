bfabf289f2a8555cb8fe8c13cf138dabb522fbcc
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-12-02 02:29:16 +0900

    refactor(RecipeListServiceImpl): 중복되는 코드 제거 #253

--- remote/src/main/java/com/kdjj/remote/service/RecipeListServiceImpl.kt
+++ remote/src/main/java/com/kdjj/remote/service/RecipeListServiceImpl.kt
@@ -14,86 +14,63 @@ internal class RecipeListServiceImpl @Inject constructor(
     private val fireStore: FirebaseFirestore
 ) : RemoteRecipeListService {
 
-    private var latestListQuery: Query? = null
-    private var popularListQuery: Query? = null
-    private var searchListQuery: Query? = null
+    private val queryMap = mutableMapOf<String, Query?>(
+        "LATEST_LIST_QUERY" to null,
+        "POPULAR_LIST_QUERY" to null,
+        "SEARCH_LIST_QUERY" to null
+    )
+
+    private suspend fun fetchRecipeList(
+        refresh: Boolean,
+        listQuery: Query,
+        key: String,
+        limitSize: Long = DEFAULT_PAGING_SIZE,
+    ): List<RecipeDto> = withContext(Dispatchers.IO) {
+        if (refresh) {
+            queryMap[key] = null
+        }
+
+        val query = queryMap[key] ?: listQuery.limit(limitSize)
+        val querySnapshot = query.get(Source.SERVER).await()
+        if (querySnapshot.documents.isNotEmpty()) {
+            queryMap[key] = query.startAfter(querySnapshot.documents.last())
+        }
+
+        querySnapshot.map { queryDocumentSnapshot ->
+            queryDocumentSnapshot.toObject()
+        }
+
+    }
 
     override suspend fun fetchLatestRecipeListAfter(
         refresh: Boolean
-    ): List<RecipeDto> =
-        withContext(Dispatchers.IO) {
-            if (refresh) {
-                latestListQuery = null
-            }
+    ): List<RecipeDto> {
+        val listQuery = fireStore.collection(RECIPE_COLLECTION_ID)
+            .orderBy(FIELD_CREATE_TIME, Query.Direction.DESCENDING)
 
-            val query = latestListQuery ?: fireStore.collection(RECIPE_COLLECTION_ID)
-                .orderBy(FIELD_CREATE_TIME, Query.Direction.DESCENDING)
-                .limit(PAGING_SIZE)
-
-            val querySnapshot = query.get(Source.SERVER).await()
-            if (querySnapshot.documents.isNotEmpty()) {
-                latestListQuery = fireStore.collection(RECIPE_COLLECTION_ID)
-                    .orderBy(FIELD_CREATE_TIME, Query.Direction.DESCENDING)
-                    .startAfter(querySnapshot.documents.last())
-                    .limit(PAGING_SIZE)
-            }
-            querySnapshot.map {
-                it.toObject<RecipeDto>()
-            }
-        }
+        return fetchRecipeList(refresh, listQuery, LATEST_LIST_QUERY)
+    }
 
     override suspend fun fetchPopularRecipeListAfter(
         refresh: Boolean
-    ): List<RecipeDto> =
-        withContext(Dispatchers.IO) {
-            if (refresh) {
-                popularListQuery = null
-            }
+    ): List<RecipeDto> {
+        val listQuery = fireStore.collection(RECIPE_COLLECTION_ID)
+            .orderBy(FIELD_VIEW_COUNT, Query.Direction.DESCENDING)
 
-            val query = popularListQuery ?: fireStore.collection(RECIPE_COLLECTION_ID)
-                .orderBy(FIELD_VIEW_COUNT, Query.Direction.DESCENDING)
-                .limit(PAGING_SIZE)
-
-            val querySnapshot = query.get(Source.SERVER).await()
-            if (querySnapshot.documents.isNotEmpty()) {
-                popularListQuery = fireStore.collection(RECIPE_COLLECTION_ID)
-                    .orderBy(FIELD_VIEW_COUNT, Query.Direction.DESCENDING)
-                    .startAfter(querySnapshot.documents.last())
-                    .limit(PAGING_SIZE)
-            }
-            querySnapshot.map {
-                it.toObject<RecipeDto>()
-            }
-        }
+        return fetchRecipeList(refresh, listQuery, POPULAR_LIST_QUERY)
+    }
 
     override suspend fun fetchSearchRecipeListAfter(
         keyword: String,
         refresh: Boolean
-    ): List<RecipeDto> =
-        withContext(Dispatchers.IO) {
-            if (refresh) {
-                searchListQuery = null
-            }
+    ): List<RecipeDto> {
+        val listQuery = fireStore.collection(RECIPE_COLLECTION_ID)
+            .whereGreaterThanOrEqualTo(FIELD_TITLE, keyword)
+            .whereLessThan(FIELD_TITLE, keyword + HANGLE_MAX_VALUE)
+            .orderBy(FIELD_TITLE, Query.Direction.ASCENDING)
 
-            val query = searchListQuery ?: fireStore.collection(RECIPE_COLLECTION_ID)
-                .whereGreaterThanOrEqualTo(FIELD_TITLE, keyword)
-                .whereLessThan(FIELD_TITLE, keyword + HANGLE_MAX_VALUE)
-                .orderBy(FIELD_TITLE, Query.Direction.ASCENDING)
-                .limit(PAGING_SIZE)
-
-            val querySnapshot = query.get(Source.SERVER).await()
-            if (querySnapshot.documents.isNotEmpty()) {
-                searchListQuery = fireStore.collection(RECIPE_COLLECTION_ID)
-                    .whereGreaterThanOrEqualTo(FIELD_TITLE, keyword)
-                    .whereLessThan(FIELD_TITLE, keyword + HANGLE_MAX_VALUE)
-                    .orderBy(FIELD_TITLE, Query.Direction.ASCENDING)
-                    .startAfter(querySnapshot.documents.last())
-                    .limit(PAGING_SIZE)
-            }
-            querySnapshot.map {
-                it.toObject<RecipeDto>()
-            }
-        }
+        return fetchRecipeList(refresh, listQuery, SEARCH_LIST_QUERY)
+    }
 
     companion object {
 
@@ -102,6 +79,9 @@ internal class RecipeListServiceImpl @Inject constructor(
         const val FIELD_CREATE_TIME = "createTime"
         const val FIELD_VIEW_COUNT = "viewCount"
         const val RECIPE_COLLECTION_ID = "recipe"
-        const val PAGING_SIZE = 10L
+        const val DEFAULT_PAGING_SIZE = 10L
+        const val LATEST_LIST_QUERY = "latestListQuery"
+        const val POPULAR_LIST_QUERY = "popularListQuery"
+        const val SEARCH_LIST_QUERY = "searchListQuery"
     }
 }
