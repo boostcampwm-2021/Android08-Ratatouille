e942593777da1852fab39054eca3d2cdd9eed029
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-12-01 23:50:53 +0900

    test(RecipeImageRemoteDataSourceImplTest): 테스트 코드 작성 #253

--- remote/src/test/java/com/kdjj/remote/datasource/RecipeImageRemoteDataSourceImplTest.kt
+++ remote/src/test/java/com/kdjj/remote/datasource/RecipeImageRemoteDataSourceImplTest.kt
@@ -1,39 +1,56 @@
 package com.kdjj.remote.datasource
 
-//class RecipeImageRemoteDataSourceImplTest {
-//
-//	private lateinit var mockFireStorageDaoImpl: FirebaseStorageServiceImpl
-//	private lateinit var recipeImageRemoteDataSourceImpl: RecipeImageRemoteDataSourceImpl
-//	private val testUriList = listOf("this is test uri")
-//	private val testByteArrayList = listOf(testUriList.first().toByteArray())
-//
-//	@Before
-//	fun setup() {
-//		mockFireStorageDaoImpl = mock(FirebaseStorageServiceImpl::class.java)
-//		recipeImageRemoteDataSourceImpl = RecipeImageRemoteDataSourceImpl(mockFireStorageDaoImpl)
-//	}
-//
-//	@Test
-//	fun fetchRecipeImage_getImageByteArray_true(): Unit = runBlocking {
-//		//given
-//		`when`(mockFireStorageDaoImpl.fetchRecipeImage(testUriList)).thenReturn(
-//			Result.success(
-//				testByteArrayList
-//			)
-//		)
-//		//when
-//		val byteArrayResult = recipeImageRemoteDataSourceImpl.fetchRecipeImage(testUriList)
-//		//then
-//		assertEquals(testByteArrayList, byteArrayResult.getOrNull())
-//	}
-//
-//	@Test
-//	fun uploadRecipeImage_getRemoteStorageImageUri_true(): Unit = runBlocking {
-//		//given
-//		`when`(mockFireStorageDaoImpl.uploadRecipeImage(testUriList.first())).thenReturn(Result.success(testUriList.first()))
-//		//when
-//		val newImagePathResult = recipeImageRemoteDataSourceImpl.uploadRecipeImage(testUriList.first())
-//		//then
-//		assertEquals(testUriList, newImagePathResult.getOrNull())
-//	}
-//}
+import com.kdjj.remote.service.RecipeImageService
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+class RecipeImageRemoteDataSourceImplTest {
+
+    private lateinit var mockRecipeImageService: RecipeImageService
+    private lateinit var mockRecipeImageRemoteDataSourceImpl: RecipeImageRemoteDataSourceImpl
+    private val dummyUriList = listOf(
+        "dummyUri1",
+        "dummyUri2",
+        "dummyUri3"
+    )
+    private val dummyByteArray = dummyUriList.map { it.toByteArray() }
+    private val dummyUploadRecipeImageUri = "dummyUploadRecipeImage"
+
+    @Before
+    fun setup() {
+        // given
+        mockRecipeImageService = mock(RecipeImageService::class.java)
+        mockRecipeImageRemoteDataSourceImpl = RecipeImageRemoteDataSourceImpl(mockRecipeImageService)
+    }
+
+    @Test
+    fun fetchRecipeImage_giveUrlList_successAndByteArray(): Unit = runBlocking {
+        // given
+        `when`(mockRecipeImageService.fetchRecipeImage(dummyUriList)).thenReturn(dummyByteArray)
+
+        // when
+        val fetchRecipeImageResult = mockRecipeImageRemoteDataSourceImpl.fetchRecipeImage(dummyUriList)
+
+        // then
+        assertTrue(fetchRecipeImageResult.isSuccess)
+        assertEquals(dummyByteArray, fetchRecipeImageResult.getOrNull())
+    }
+
+    @Test
+    fun uploadRecipeImage_giveUri_successAndUri(): Unit = runBlocking {
+        // given
+        `when`(mockRecipeImageService.uploadRecipeImage(dummyUriList.first())).thenReturn(dummyUploadRecipeImageUri)
+
+        // when
+        val uploadRecipeImageResult = mockRecipeImageRemoteDataSourceImpl.uploadRecipeImage(dummyUriList.first())
+
+        // then
+        assertTrue(uploadRecipeImageResult.isSuccess)
+        assertEquals(dummyUploadRecipeImageUri, uploadRecipeImageResult.getOrNull())
+    }
+}
