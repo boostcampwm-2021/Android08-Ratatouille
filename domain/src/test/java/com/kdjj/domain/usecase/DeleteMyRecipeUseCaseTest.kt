82943999a00413d0b684a612e55c77ff696fa68f
dongkey <utdhkim94@gmail.com>
2021-12-01 23:57:32 +0900

    test: test DeleteMyRecipeUseCase #254

---
+++ domain/src/test/java/com/kdjj/domain/usecase/DeleteMyRecipeUseCaseTest.kt
@@ -0,0 +1,38 @@
+package com.kdjj.domain.usecase
+
+import com.kdjj.domain.model.request.DeleteMyRecipeRequest
+import com.kdjj.domain.repository.RecipeRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.lang.Exception
+
+class DeleteMyRecipeUseCaseTest {
+
+    private lateinit var mockRecipeRepository: RecipeRepository
+    private lateinit var mockDeleteMyRecipeRequest: DeleteMyRecipeRequest
+    private lateinit var deleteMyRecipeUseCase: DeleteMyRecipeUseCase
+
+    @Before
+    fun setup() {
+        mockRecipeRepository = mock(RecipeRepository::class.java)
+        mockDeleteMyRecipeRequest = mock(DeleteMyRecipeRequest::class.java)
+        deleteMyRecipeUseCase = DeleteMyRecipeUseCase(mockRecipeRepository)
+    }
+
+    @Test
+    fun deleteMyRecipeUseCase(): Unit = runBlocking {
+        //given
+        `when`(mockRecipeRepository.deleteMyRecipe(mockDeleteMyRecipeRequest.recipe))
+            .thenReturn(Result.success(Unit))
+
+        //when
+        val testResult = deleteMyRecipeUseCase.invoke(mockDeleteMyRecipeRequest)
+
+        //then
+        assertEquals(Result.success(Unit), testResult)
+    }
+}
